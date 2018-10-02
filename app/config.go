f0d8f0578e63d8878aed326dea97461d58e059dd
Damien Leroy <damien.leroy@keemotion.com>
2018-10-02 21:11:43 +0200

    improve config

--- app/config.go
+++ app/config.go
@@ -11,14 +11,24 @@ import (
 var ConfigFile = "conf/default.yaml"
 
 // LoadConfig does load the app configuration in viper
+// The precedence of config values in viper is the following:
+// 1) explicit call to Set
+// 2) flag (i.e., settable by command line)
+// 3) env
+// 4) config file
+// 5) key/value store
+// 6) default
 func LoadConfig() {
 
+	// through env variables
+	viper.SetEnvPrefix("algorea") // env variables must be prefixed by "ALGOREA_"
+	viper.AutomaticEnv()          // read in environment variables
+
+	// through the config file
 	viper.SetConfigFile(ConfigFile)
-
-	viper.AutomaticEnv() // read in environment variables that match
-
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println("Cannot read config:", err)
 		os.Exit(1)
 	}
+
 }
