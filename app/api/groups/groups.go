63456bd50f5aa9e1906fb3aee8c929571b1ca4be
Damien Leroy <dleroy@smad.be>
2018-11-23 18:56:11 +0100

    re-replace tab by spaces in some remaining files

--- app/api/groups/groups.go
+++ app/api/groups/groups.go
@@ -1,23 +1,23 @@
 package groups
 
 import (
-	"github.com/France-ioi/AlgoreaBackend/app/database"
-	s "github.com/France-ioi/AlgoreaBackend/app/service"
+  "github.com/France-ioi/AlgoreaBackend/app/database"
+  s "github.com/France-ioi/AlgoreaBackend/app/service"
 
-	"github.com/go-chi/chi"
+  "github.com/go-chi/chi"
 )
 
 // Service is the mount point for services related to `groups`
 type Service struct {
-	Store *database.DataStore
+  Store *database.DataStore
 }
 
 // New creates a service context
 func New(store *database.DataStore) *Service {
-	return &Service{store}
+  return &Service{store}
 }
 
 // AppendRoutes adds the routes of this pakcage to the parent router
 func (srv *Service) AppendRoutes(router *chi.Mux) {
-	router.Get("/groups/", s.AppHandler(srv.getAll).ServeHTTP)
+  router.Get("/groups/", s.AppHandler(srv.getAll).ServeHTTP)
 }
--- app/types/validation.go
+++ app/types/validation.go
@@ -4,16 +4,16 @@ package types
 
 // Validatable is the interface indicating the type implementing it supports data validation.
 type validatable interface {
-	// Validate validates the data and returns an error if validation fails.
-	Validate() error
+  // Validate validates the data and returns an error if validation fails.
+  Validate() error
 }
 
 // Validate checks a set of `Validatable` values and returns the first encountered error, or nil
 func Validate(values ...validatable) (err error) {
-	for _, value := range values {
-		if err = value.Validate(); err != nil {
-			return
-		}
-	}
-	return
+  for _, value := range values {
+    if err = value.Validate(); err != nil {
+      return
+    }
+  }
+  return
 }
