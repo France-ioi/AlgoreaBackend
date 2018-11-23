acbe6ffc23533642b9975c99bc482f7bedf719b6
Damien Leroy <dleroy@smad.be>
2018-11-23 18:18:19 +0100

    move api to parent directory

--- README.md
+++ README.md
@@ -52,6 +52,6 @@ go lint
 ### Routing a request
 
 * The web app is defined in `app.go` which loads all the middlewares and routes. The routing part consists in mounting the API on `/` and giving a context to it (i.e., the app database connection)
-* The API routing (`app/service/api/api.go`) does the same for mounting all group of services.
-* A service group (e.g., `app/service/api/groups/groups.go`.) mounts all its services and pass the context again.
-* Each service has its dedicated file (e.g., `app/service/api/groups/get-all.go`). We try to separate the actual HTTP request parsing and response generation from the actual business logic and the call to the database.
+* The API routing (`app/api/api.go`) does the same for mounting all group of services.
+* A service group (e.g., `app/api/groups/groups.go`.) mounts all its services and pass the context again.
+* Each service has its dedicated file (e.g., `app/api/groups/get-all.go`). We try to separate the actual HTTP request parsing and response generation from the actual business logic and the call to the database.
--- app/app.go
+++ app/app.go
@@ -1,66 +1,66 @@
 package app
 
 import (
-  "math/rand"
-  "time"
+	"math/rand"
+	"time"
 
-  "github.com/France-ioi/AlgoreaBackend/app/config"
-  "github.com/France-ioi/AlgoreaBackend/app/database"
-  "github.com/France-ioi/AlgoreaBackend/app/service/api"
+	"github.com/France-ioi/AlgoreaBackend/app/api"
+	"github.com/France-ioi/AlgoreaBackend/app/config"
+	"github.com/France-ioi/AlgoreaBackend/app/database"
 
-  "github.com/go-chi/chi"
-  "github.com/go-chi/chi/middleware"
-  "github.com/go-chi/render"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 // Application is the core state of the app
 type Application struct {
-  HTTPHandler *chi.Mux
-  Config      *config.Root
-  Database    *database.DB
+	HTTPHandler *chi.Mux
+	Config      *config.Root
+	Database    *database.DB
 }
 
 // New configures application resources and routes.
 func New() (*Application, error) {
-  var err error
+	var err error
 
-  var conf *config.Root
-  if conf, err = config.Load(); err != nil {
-    return nil, err
-  }
+	var conf *config.Root
+	if conf, err = config.Load(); err != nil {
+		return nil, err
+	}
 
-  // Init the PRNG with current time
-  rand.Seed(time.Now().UTC().UnixNano())
+	// Init the PRNG with current time
+	rand.Seed(time.Now().UTC().UnixNano())
 
-  logger := NewLogger()
+	logger := NewLogger()
 
-  var db *database.DB
-  if db, err = database.DBConn(conf.Database); err != nil {
-    logger.WithField("module", "database").Error(err)
-    return nil, err
-  }
+	var db *database.DB
+	if db, err = database.DBConn(conf.Database); err != nil {
+		logger.WithField("module", "database").Error(err)
+		return nil, err
+	}
 
-  var apiCtx *api.Ctx
-  if apiCtx, err = api.NewCtx(conf, db); err != nil {
-    logger.Error(err)
-    return nil, err
-  }
+	var apiCtx *api.Ctx
+	if apiCtx, err = api.NewCtx(conf, db); err != nil {
+		logger.Error(err)
+		return nil, err
+	}
 
-  // Set up middlewares
-  router := chi.NewRouter()
+	// Set up middlewares
+	router := chi.NewRouter()
 
-  router.Use(middleware.Recoverer)
-  router.Use(middleware.RequestID)
-  router.Use(middleware.RealIP)
-  router.Use(middleware.DefaultCompress)
-  router.Use(middleware.Timeout(time.Duration(conf.Timeout) * time.Second))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.DefaultCompress)
+	router.Use(middleware.Timeout(time.Duration(conf.Timeout) * time.Second))
 
-  router.Use(NewStructuredLogger(logger))
-  router.Use(render.SetContentType(render.ContentTypeJSON))
+	router.Use(NewStructuredLogger(logger))
+	router.Use(render.SetContentType(render.ContentTypeJSON))
 
-  router.Use(corsConfig().Handler) // no need for CORS if served through the same domain
+	router.Use(corsConfig().Handler) // no need for CORS if served through the same domain
 
-  router.Mount("/", apiCtx.Router())
+	router.Mount("/", apiCtx.Router())
 
-  return &Application{router, conf, db}, nil
+	return &Application{router, conf, db}, nil
 }
