3f56bbf61b9a449bc6cc996d9f9d8beb02fe6c58
Paweł Słomka <pslomka@pslomka.com>
2018-12-07 23:39:33 +0100

    Add BDD tests for breadcrumbs
    - go fmt all affected files,
    - add missing test scenarios
    - refactor access result check function
    - add support for partial access in check function

--- app/database/itemStore.go
+++ app/database/itemStore.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/France-ioi/AlgoreaBackend/app/auth"
 	"github.com/France-ioi/AlgoreaBackend/app/logging"
 	t "github.com/France-ioi/AlgoreaBackend/app/types"
@@ -9,6 +11,13 @@ import (
 // ItemStore implements database operations on items
 type ItemStore struct {
 	*DataStore
+}
+
+type itemAccessDetails struct {
+	ItemID        int64 `sql:"column:idItem"`
+	FullAccess    bool  `sql:"column:fullAccess"`
+	PartialAccess bool  `sql:"column:partialAccess"`
+	GrayedAccess  bool  `sql:"column:grayedAccess"`
 }
 
 // Item matches the content the `items` table
@@ -65,42 +74,52 @@ func (s *ItemStore) IsValidHierarchy(ids []int64) (bool, error) {
 // ValidateUserAccess gets a set of item ids and returns whether the given user is authorized to see them all
 func (s *ItemStore) ValidateUserAccess(user *auth.User, itemIDs []int64) (bool, error) {
 
-	accessResult := []struct {
-		ItemID       int64 `sql:"column:idItem"`
-		FullAccess   bool  `sql:"column:fullAccess"`
-		GrayedAccess bool  `sql:"column:grayedAccess"`
-	}{}
-
+	var accDets []itemAccessDetails
 	db := s.GroupItems().MatchingUserAncestors(user).
-		Select("idItem, MAX(bCachedFullAccess) AS fullAccess, MAX(bCachedGrayedAccess) AS grayedAccess").
+		Select("idItem, MAX(bCachedFullAccess) AS fullAccess, MAX(bCachedPartialAccess) AS partialAccess, MAX(bCachedGrayedAccess) AS grayedAccess").
 		Where("groups_items.idItem IN (?)", itemIDs).
-		Group("idItem")
-	db = db.Scan(&accessResult)
+		Group("idItem").Scan(&accDets)
 	if db.Error != nil {
 		return false, db.Error
 	}
 
-	// check for each id whether it has access
-	// must have full or partial access, or for the last one, grayed access
+	if err := checkAccess(itemIDs, accDets); err != nil {
+		logging.Logger.Infof("User access validation failed: %v", err)
+		return false, nil
+	}
+	return true, nil
+}
+
+// checkAccess checks if the user has access to all items:
+// - user has to have full access to all items
+// OR
+// - user has to have full access to all but last, and grayed access to that last item.
+func checkAccess(itemIDs []int64, accDets []itemAccessDetails) error {
 	for i, id := range itemIDs {
-		found := false
-		last := i == len(itemIDs)-1 // find the last id in the hierarchy
-		for _, result := range accessResult {
-			if result.ItemID == id {
-				found = true
-				if result.FullAccess || (last && result.GrayedAccess) {
-					// ok
-				} else {
-					logging.Logger.Infof("User access validation failed: not enough perm on item_id %d", id)
-					return false, nil // not enough access
-				}
-			}
-		}
-		if !found {
-			logging.Logger.Infof("User access validation failed: not visible item_id %d", id)
-			return false, nil // no row matching this item_id
+		last := (i == len(itemIDs)-1)
+		if err := checkAccessForID(id, last, accDets); err != nil {
+			return err
 		}
 	}
+	return nil
+}
 
-	return true, nil
+func checkAccessForID(id int64, last bool, accDets []itemAccessDetails) error {
+	for _, res := range accDets {
+		if res.ItemID != id {
+			continue
+		}
+		if res.FullAccess || res.PartialAccess {
+			// OK, user has full access.
+			return nil
+		}
+		if res.GrayedAccess && last {
+			// OK, user has grayed access on the last item.
+			return nil
+		}
+		return fmt.Errorf("not enough perm on item_id %d", id)
+	}
+
+	// no row matching this item_id
+	return fmt.Errorf("not visible item_id %d", id)
 }
