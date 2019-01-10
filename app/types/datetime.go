783731457040fb88263843df4713b49768af39c6
Damien Leroy <dleroy@smad.be>
2019-01-10 16:44:14 +0100

    add datetime type

---
+++ app/types/datetime.go
@@ -0,0 +1,76 @@
+package types
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+)
+
+// Doc is mainly in the "int64" file :-)
+
+type (
+	// Datetime is a date+time format which can be set/not-set and null/not-null
+	Datetime struct {
+		Value time.Time
+		Set   bool
+		Null  bool
+	}
+	// RequiredDatetime must be set and not null
+	RequiredDatetime struct{ Datetime }
+	// NullableDatetime must be set and can be null
+	NullableDatetime struct{ Datetime }
+	// OptionalDatetime can be not set. If set, cannot be null.
+	OptionalDatetime struct{ Datetime }
+	// OptNullDatetime can be not set or null
+	OptNullDatetime struct{ Datetime }
+)
+
+// NewDatetime creates a Datetime which is not-null and set with the given value
+func NewDatetime(t time.Time) *Datetime {
+	n := &Datetime{}
+	n.Value = t
+	n.Set = true
+	n.Null = false
+	return n
+}
+
+// UnmarshalJSON parse JSON data to the type
+func (t *Datetime) UnmarshalJSON(data []byte) (err error) {
+	t.Set = true // If this method was called, the value was set.
+	t.Null = (string(data) == jsonNull)
+	var temp time.Time
+	err = json.Unmarshal(data, &temp)
+	if err == nil {
+		t.Value = temp
+	}
+	return
+}
+
+// Validate checks that the subject matches "required" (set and not-null)
+func (t *RequiredDatetime) Validate() error {
+	if !t.Set || t.Null {
+		return errors.New("must be given and not null")
+	}
+	return nil
+}
+
+// Validate checks that the subject matches "nullable" (must be set)
+func (t *NullableDatetime) Validate() error {
+	if !t.Set {
+		return errors.New("must be given")
+	}
+	return nil
+}
+
+// Validate checks that the subject matches "optional" (not-null)
+func (t *OptionalDatetime) Validate() error {
+	if t.Null {
+		return errors.New("must not be null")
+	}
+	return nil
+}
+
+// Validate checks that the subject matches "optnull" (always true)
+func (t *OptNullDatetime) Validate() error {
+	return nil
+}
---
+++ app/types/datetime_test.go
@@ -0,0 +1,77 @@
+package types
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	assert_lib "github.com/stretchr/testify/assert"
+)
+
+type SampleDTInput struct {
+	CreatedAt   RequiredDatetime
+	ValidFrom   NullableDatetime
+	ModifiedAt  OptionalDatetime
+	AccessUntil OptNullDatetime
+}
+
+func (v *SampleDTInput) validate() error {
+	return Validate(&v.CreatedAt, &v.ValidFrom, &v.ModifiedAt, &v.AccessUntil)
+}
+
+func TestNewDT(t *testing.T) {
+	assert := assert_lib.New(t)
+
+	value := time.Date(2001, time.February, 3, 5, 6, 7, 890000000, time.UTC)
+	n := NewDatetime(value)
+	assert.Equal(value, n.Value)
+	assert.True(n.Set)
+	assert.False(n.Null)
+}
+
+func TestDTValid(t *testing.T) {
+	assert := assert_lib.New(t)
+
+	jsonInput := `{ "CreatedAt": "2001-02-03T05:06:07.89Z", "ValidFrom": "2002-01-01T23:11:11.000000001+02:00", "ModifiedAt": "2001-09-02T12:30:00Z", "AccessUntil": "2042-12-31T23:59:59Z" }`
+	input := &SampleDTInput{}
+	assert.NoError(json.Unmarshal([]byte(jsonInput), &input))
+	assert.Equal(time.Date(2001, time.February, 3, 5, 6, 7, 890000000, time.UTC), input.CreatedAt.Value)
+	assert.Equal(time.Date(2002, time.January, 1, 21, 11, 11, 1, time.UTC), input.ValidFrom.Value.In(time.UTC))
+	assert.Equal(time.Date(2001, time.September, 2, 12, 30, 0, 0, time.UTC), input.ModifiedAt.Value)
+	assert.Equal(time.Date(2042, time.December, 31, 23, 59, 59, 0, time.UTC), input.AccessUntil.Value)
+	assert.NoError(input.validate())
+}
+
+func TestDTWithNonDT(t *testing.T) {
+	assert := assert_lib.New(t)
+
+	jsonInput := `{ "CreatedAt": "2001", "ValidFrom": "2002-01-01T23:11:11Z", "ModifiedAt": "2001-09-02T12:30Z", "AccessUntil": "2042-12-31T23:59:59Z" }`
+	input := &SampleDTInput{}
+	assert.Error(json.Unmarshal([]byte(jsonInput), &input))
+}
+
+func TestDTWithNull(t *testing.T) {
+	assert := assert_lib.New(t)
+
+	jsonInput := `{ "CreatedAt": null, "ValidFrom": null, "ModifiedAt": null, "AccessUntil": null }`
+	input := &SampleDTInput{}
+	assert.NoError(json.Unmarshal([]byte(jsonInput), &input))
+	assert.Error(input.CreatedAt.Validate())
+	assert.NoError(input.ValidFrom.Validate())
+	assert.Error(input.ModifiedAt.Validate())
+	assert.NoError(input.AccessUntil.Validate())
+	assert.Error(input.validate())
+}
+
+func TestDTWithNotSet(t *testing.T) {
+	assert := assert_lib.New(t)
+
+	jsonInput := `{}`
+	input := &SampleDTInput{}
+	assert.NoError(json.Unmarshal([]byte(jsonInput), &input))
+	assert.Error(input.CreatedAt.Validate())
+	assert.Error(input.ValidFrom.Validate())
+	assert.NoError(input.ModifiedAt.Validate())
+	assert.NoError(input.AccessUntil.Validate())
+	assert.Error(input.validate())
+}
