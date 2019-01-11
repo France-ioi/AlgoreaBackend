333d5855b892927a405f1a7dea22b315f37726a4
Damien Leroy <dleroy@smad.be>
2019-01-11 10:30:48 +0100

    remove cached fields for now as they should be managed by a listener instead

--- app/database/groupItemStore.go
+++ app/database/groupItemStore.go
@@ -11,15 +11,14 @@ type GroupItemStore struct {
 
 // GroupItem matches the content the `groups_items` table
 type GroupItem struct {
-	ID               types.Int64    `sql:"column:ID"`
-	GroupID          types.Int64    `sql:"column:idGroup"`
-	ItemID           types.Int64    `sql:"column:idItem"`
-	CreatorUserID    types.Int64    `sql:"column:idUserCreated"`
-	FullAccessDate   types.Datetime `sql:"column:sFullAccessDate"`
-	OwnerAccess      types.Bool     `sql:"column:bOwnerAccess"`
-	ManagerAccess    types.Bool     `sql:"column:bManagerAccess"`
-	CachedFullAccess bool           `sql:"column:bCachedFullAccess"` // use Go default in DB (to be fixed)
-	Version          int64          `sql:"column:iVersion"`          // use Go default in DB (to be fixed)
+	ID             types.Int64    `sql:"column:ID"`
+	GroupID        types.Int64    `sql:"column:idGroup"`
+	ItemID         types.Int64    `sql:"column:idItem"`
+	CreatorUserID  types.Int64    `sql:"column:idUserCreated"`
+	FullAccessDate types.Datetime `sql:"column:sFullAccessDate"`
+	OwnerAccess    types.Bool     `sql:"column:bOwnerAccess"`
+	ManagerAccess  types.Bool     `sql:"column:bManagerAccess"`
+	Version        int64          `sql:"column:iVersion"` // use Go default in DB (to be fixed)
 }
 
 func (s *GroupItemStore) tableName() string {
