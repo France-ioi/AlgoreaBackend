60c01a95793aa41ab6d224241eb63d063e6633f9
Damien Leroy <dleroy@smad.be>
2019-01-28 15:33:07 +0100

    adapt Makefile & CI config for new test org

--- Makefile
+++ Makefile
@@ -13,39 +13,44 @@ ifndef BIN_DIR # to allow BIN_DIR to be given as args (see CI)
 	BIN_DIR=$(FIRSTGOPATH)/bin
 endif
 GODOG=$(BIN_DIR)/godog
-GO_JUNIT_REPORT=$(BIN_DIR)/go-junit-report
 GOMETALINTER=./bin/gometalinter
+
+# use the NOTVERBOSE env var to disable verbosity on make test
+ifneq ("$(NOT_VERBOSE)","1")
+	Q :=
+	vecho = @echo
+else
+	Q := @
+	vecho = @true
+endif
 
 .PHONY: all build test lint clean deps print-deps
 
 all: build
 build:
-	$(GOBUILD) -o $(BINARY_NAME) -v
+	$(GOBUILD) -o $(BINARY_NAME) -v -race
+test: $(TEST_REPORT_DIR)
+	$(Q)# the tests using the db do not currently support parallism
+	$(Q)$(GOTEST) -race -coverprofile=$(TEST_REPORT_DIR)/coverage.txt -covermode=atomic -v ./app/... -p 1 -parallel 1
 test-unit:
-	$(GOTEST) -race -cover -v ./app/...
+	TESTS_NODB=1 $(GOTEST) -race -cover -v ./app/...
 test-bdd: $(GODOG)
 	# to pass args: make ARGS="--tags=wip" test-bdd
-	$(GODOG) --format=progress $(ARGS)
-test-unit-report: $(GO_JUNIT_REPORT)
-	mkdir -p $(TEST_REPORT_DIR)/go-test
-	$(GOTEST) -race  -v -coverprofile=$(TEST_REPORT_DIR)/coverage.txt -covermode=atomic ./app/... 2>&1 | $(GO_JUNIT_REPORT) > $(TEST_REPORT_DIR)/go-test/junit.xml
-test-bdd-report: $(GODOG)
-	mkdir -p $(TEST_REPORT_DIR)/cucumber
-	$(GODOG) --format=junit > $(TEST_REPORT_DIR)/cucumber/junit.xml
-test: test-unit test-bdd
+	$(GODOG) --format=progress $(ARGS) .
 lint: $(GOMETALINTER)
 	PATH=./bin:$(PATH) GO111MODULE=off $(GOMETALINTER) ./... --deadline=90s
 clean:
 	$(GOCLEAN)
+	$(GOCLEAN) -testcache
 	rm -f $(BINARY_NAME)
 	rm -rf ./bin
 deps:
 	GO111MODULE=off $(GOGET) -t ./...
 print-deps:
 	$(GOLIST) -f {{.Deps}} && $(GOLIST) -f {{.TestImports}} ./...
+$(TEST_REPORT_DIR):
+	mkdir -p $(TEST_REPORT_DIR)
 $(GODOG):
 	$(GOGET) -u github.com/DATA-DOG/godog/cmd/godog
-$(GO_JUNIT_REPORT):
-	$(GOGET) -u github.com/jstemmer/go-junit-report
 $(GOMETALINTER):
 	curl -L https://git.io/vp6lP | sh /dev/stdin -b ./bin
--- bdd_test.go
+++ bdd_test.go
@@ -1,63 +1,12 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"testing"
-
 	"github.com/DATA-DOG/godog"
-	"github.com/DATA-DOG/godog/colors"
 
 	"github.com/France-ioi/AlgoreaBackend/testhelpers"
 )
 
-/*
-This file allows the BDD (gherkin/cucumber) tests to be executed with `go test`.
-`godog` is the name of the package we use to write Cucumber tests for Go.
-You can use the options (flags) of the `godog` command by prefixing them with "godog.",
-e.g., "go test -v --godog.format=progress --godog.random --godog.tags=wip"
-*/
-
-var opt = godog.Options{Output: colors.Colored(os.Stdout)}
-
-func init() {
-	godog.BindFlags("godog.", flag.CommandLine, &opt)
-}
-
-func TestMain(m *testing.M) {
-
-	flag.Parse()
-	opt.Paths = flag.Args()
-
-	status := godog.RunWithOptions("godogs", func(s *godog.Suite) {
-		FeatureContext(s)
-	}, opt)
-
-	if st := m.Run(); st > status {
-		status = st
-	}
-	os.Exit(status)
-}
-
-// FeatureContext binds the supported steps to the verifying functions
-func FeatureContext(s *godog.Suite) {
-	ctx := &testhelpers.TestContext{}
-	s.BeforeScenario(ctx.SetupTestContext)
-
-	s.Step(`^the database has the following table \'([\w\-_]*)\':$`, ctx.DBHasTable)
-	s.Step(`^a server is running as fallback$`, ctx.RunFallbackServer)
-	s.Step(`^I am the user with ID "([^"]*)"$`, ctx.IAmUserWithID)
-	s.Step(`^the time now is "([^"]*)"$`, ctx.TimeNow)
-
-	s.Step(`^I send a (GET|POST|PUT|DELETE) request to "([^"]*)"$`, ctx.ISendrequestTo)
-	s.Step(`^I send a (GET|POST|PUT|DELETE) request to "([^"]*)" with the following body:$`, ctx.ISendrequestToWithBody)
-	s.Step(`^the response code should be (\d+)$`, ctx.TheResponseCodeShouldBe)
-	s.Step(`^the response body should be, in JSON:$`, ctx.TheResponseBodyShouldBeJSON)
-	s.Step(`^the response header "([^"]*)" should be "([^"]*)"$`, ctx.TheResponseHeaderShouldBe)
-	s.Step(`^the response error message should contain "([^"]*)"$`, ctx.TheResponseErrorMessageShouldContain)
-	s.Step(`^it should be a JSON array with (\d+) entr(ies|y)$`, ctx.ItShouldBeAJSONArrayWithEntries)
-	s.Step(`^the table "([^"]*)" should be:$`, ctx.TableShouldBe)
-	s.Step(`^the table "([^"]*)" at ID "(\d*)" should be:$`, ctx.TableAtIDShouldBe)
-
-	s.AfterScenario(ctx.ScenarioTeardown)
+// FeatureContext is required to be able to use the `godog` command at root of the project
+func FeatureContext(s *godog.Suite) { // nolint: megacheck, deadcode
+	testhelpers.FeatureContext(s)
 }
