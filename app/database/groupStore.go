0b5da9331ed042c0166e4f3dbb071f9f5bc04018
Dmitry Zenovich <dzenovich@gmail.com>
2019-02-21 20:18:52 +0300

    remove unneeded GroupStore.ByID()

--- app/database/groupStore.go
+++ app/database/groupStore.go
@@ -5,11 +5,6 @@ type GroupStore struct {
 	*DataStore
 }
 
-// ByID returns a composable query of groups filtered by groupID
-func (s *GroupStore) ByID(groupID int64) *DB {
-	return s.Where("groups.ID = ?", groupID)
-}
-
 // OwnedBy returns a composable query for getting all the groups
 // that are descendants of the user's owned group using AuthUser object
 func (s *GroupStore) OwnedBy(user AuthUser) *DB {
