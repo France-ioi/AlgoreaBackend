e940786ad910c5bb309e5e06f27f2f521e5196f6
Dmitry Zenovich <dzenovich@gmail.com>
2019-02-21 18:34:30 +0300

    do not export database.DB and stop proxying methods to gorm (*gorm.DB -> db *gorm.DB)

--- app/database/db.go
+++ app/database/db.go
@@ -13,7 +13,7 @@ import (
 
 // DB contains information for current db connection (wraps *gorm.DB)
 type DB struct {
-	*gorm.DB
+	db *gorm.DB
 }
 
 // NewDB wraps *gorm.DB
@@ -37,7 +37,7 @@ func Open(dsnConfig string) (*DB, error) {
 }
 
 func (conn *DB) inTransaction(txFunc func(*DB) error) (err error) {
-	var txDB = conn.Begin()
+	var txDB = conn.db.Begin()
 	if err != nil {
 		return err
 	}
@@ -63,39 +63,39 @@ func (conn *DB) inTransaction(txFunc func(*DB) error) (err error) {
 
 // Limit specifies the number of records to be retrieved
 func (conn *DB) Limit(limit interface{}) *DB {
-	return NewDB(conn.DB.Limit(limit))
+	return NewDB(conn.db.Limit(limit))
 }
 
 // Where returns a new relation, filters records with given conditions, accepts `map`, `struct` or `string` as conditions, refer http://jinzhu.github.io/gorm/crud.html#query
 func (conn *DB) Where(query interface{}, args ...interface{}) *DB {
-	return NewDB(conn.DB.Where(query, args...))
+	return NewDB(conn.db.Where(query, args...))
 }
 
 // Joins specifies Joins conditions
 //     db.Joins("JOIN emails ON emails.user_id = users.id AND emails.email = ?", "jinzhu@example.org").Find(&user)
 func (conn *DB) Joins(query string, args ...interface{}) *DB {
-	return NewDB(conn.DB.Joins(query, args...))
+	return NewDB(conn.db.Joins(query, args...))
 }
 
 // Or filters records that match before conditions or this one, similar to `Where`
 func (conn *DB) Or(query interface{}, args ...interface{}) *DB {
-	return NewDB(conn.DB.Or(query, args...))
+	return NewDB(conn.db.Or(query, args...))
 }
 
 // Select specifies fields that you want to retrieve from database when querying, by default, will select all fields;
 // When creating/updating, specify fields that you want to save to database
 func (conn *DB) Select(query interface{}, args ...interface{}) *DB {
-	return NewDB(conn.DB.Select(query, args...))
+	return NewDB(conn.db.Select(query, args...))
 }
 
 // Table specifies the table you would like to run db operations
 func (conn *DB) Table(name string) *DB {
-	return NewDB(conn.DB.Table(name))
+	return NewDB(conn.db.Table(name))
 }
 
 // Group specifies the group method on the find
 func (conn *DB) Group(query string) *DB {
-	return NewDB(conn.DB.Group(query))
+	return NewDB(conn.db.Group(query))
 }
 
 // Order specifies order when retrieve records from database, set reorder to `true` to overwrite defined conditions
@@ -103,60 +103,60 @@ func (conn *DB) Group(query string) *DB {
 //     db.Order("name DESC", true) // reorder
 //     db.Order(gorm.Expr("name = ? DESC", "first")) // sql expression
 func (conn *DB) Order(value interface{}, reorder ...bool) *DB {
-	return NewDB(conn.DB.Order(value, reorder...))
+	return NewDB(conn.db.Order(value, reorder...))
 }
 
 // Having specifies HAVING conditions for GROUP BY
 func (conn *DB) Having(query interface{}, args ...interface{}) *DB {
-	return NewDB(conn.DB.Having(query, args...))
+	return NewDB(conn.db.Having(query, args...))
 }
 
 // Union specifies UNION of two queries (receiver UNION query)
 func (conn *DB) Union(query interface{}) *DB {
-	return NewDB(conn.DB.New().Raw("? UNION ?", conn.DB.QueryExpr(), query))
+	return NewDB(conn.db.New().Raw("? UNION ?", conn.db.QueryExpr(), query))
 }
 
 // UnionAll specifies UNION ALL of two queries (receiver UNION ALL query)
 func (conn *DB) UnionAll(query interface{}) *DB {
-	return NewDB(conn.DB.New().Raw("? UNION ALL ?", conn.DB.QueryExpr(), query))
+	return NewDB(conn.db.New().Raw("? UNION ALL ?", conn.db.QueryExpr(), query))
 }
 
 // Raw uses raw sql as conditions
 //    db.Raw("SELECT name, age FROM users WHERE name = ?", 3).Scan(&result)
 func (conn *DB) Raw(query string, args ...interface{}) *DB {
 	// db.Raw("").Joins(...) is a hack for making db.Raw("...").Joins(...) work better
-	return NewDB(conn.DB.Raw("").Joins(query, args...))
+	return NewDB(conn.db.Raw("").Joins(query, args...))
 }
 
 // SubQuery returns the query as sub query
 func (conn *DB) SubQuery() interface{} {
-	return conn.DB.SubQuery()
+	return conn.db.SubQuery()
 }
 
 // QueryExpr returns the query as expr object
 func (conn *DB) QueryExpr() interface{} {
-	return conn.DB.QueryExpr()
+	return conn.db.QueryExpr()
 }
 
 // Scan scans value to a struct
 func (conn *DB) Scan(dest interface{}) *DB {
-	return NewDB(conn.DB.Scan(dest))
+	return NewDB(conn.db.Scan(dest))
 }
 
 // ScanIntoSliceOfMaps scans value into a slice of maps
 func (conn *DB) ScanIntoSliceOfMaps(dest *[]map[string]interface{}) *DB {
-	rows, err := conn.DB.Rows()
-	if conn.DB.AddError(err) != nil {
+	rows, err := conn.db.Rows()
+	if conn.db.AddError(err) != nil {
 		return conn
 	}
 	cols, err := rows.Columns()
-	if conn.DB.AddError(err) != nil {
+	if conn.db.AddError(err) != nil {
 		return conn
 	}
 
 	if rows != nil {
 		defer func() {
-			if conn.DB.AddError(rows.Close()) != nil {
+			if conn.db.AddError(rows.Close()) != nil {
 				return
 			}
 		}()
@@ -188,17 +188,17 @@ func (conn *DB) ScanIntoSliceOfMaps(dest *[]map[string]interface{}) *DB {
 
 // Count gets how many records for a model
 func (conn *DB) Count(dest interface{}) *DB {
-	return NewDB(conn.DB.Count(dest))
+	return NewDB(conn.db.Count(dest))
 }
 
 // Take returns a record that match given conditions, the order will depend on the database implementation
 func (conn *DB) Take(out interface{}, where ...interface{}) *DB {
-	return NewDB(conn.DB.Take(out, where...))
+	return NewDB(conn.db.Take(out, where...))
 }
 
 // Error returns current errors
 func (conn *DB) Error() error {
-	return conn.DB.Error
+	return conn.db.Error
 }
 
 // insert reads fields from the data struct and insert the values which have been set
@@ -249,5 +249,5 @@ func (conn *DB) insert(tableName string, data interface{}) error {
 		}
 	}
 	query := fmt.Sprintf("INSERT INTO `%s` (%s) VALUES (%s)", tableName, strings.Join(attributes, ", "), strings.Join(valueMarks, ", ")) // nolint: gosec
-	return conn.Exec(query, values...).Error
+	return conn.db.Exec(query, values...).Error
 }
