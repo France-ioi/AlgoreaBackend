b5e832f790650ca4688bf79932952a3b45290cfb
Dmitry Zenovich <dzenovich@gmail.com>
2019-02-21 12:12:11 +0300

    add a test for ResolveURLQueryPathInt64Field

--- app/service/utils_test.go
+++ app/service/utils_test.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/go-chi/chi"
 
 	assertlib "github.com/stretchr/testify/assert"
 )
@@ -72,4 +75,75 @@ func TestQueryParamToInt64Slice(t *testing.T) {
 		})
 	}
 
+}
+
+func TestResolveURLQueryPathInt64Field(t *testing.T) {
+	testCases := []struct {
+		desc           string
+		routeString    string
+		queryString    string
+		expectedValue  int64
+		expectedErrMsg string
+	}{
+		{
+			desc:           "single value",
+			routeString:    "/{id}",
+			queryString:    "/3",
+			expectedValue:  3,
+			expectedErrMsg: "",
+		},
+		{
+			desc:           "multiple value",
+			routeString:    "/{id}",
+			queryString:    "/4,5",
+			expectedValue:  0,
+			expectedErrMsg: "missing id",
+		},
+		{
+			desc:           "not a int64 (string)",
+			routeString:    "/{id}",
+			queryString:    "/word",
+			expectedValue:  0,
+			expectedErrMsg: "missing id",
+		},
+		{
+			desc:           "not a int64 (empty val)",
+			routeString:    "/{id}/",
+			queryString:    "//",
+			expectedValue:  0,
+			expectedErrMsg: "missing id",
+		},
+		{
+			desc:           "too big for int64",
+			routeString:    "/{id}",
+			queryString:    "/123456789012345678901234567890",
+			expectedValue:  0,
+			expectedErrMsg: "missing id",
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.desc, func(t *testing.T) {
+			assert := assertlib.New(t)
+
+			r := chi.NewRouter()
+			called := false
+			handler := func(w http.ResponseWriter, r *http.Request) {
+				called = true
+				value, err := ResolveURLQueryPathInt64Field(r, "id")
+				if testCase.expectedErrMsg != "" {
+					assert.EqualError(err, testCase.expectedErrMsg)
+				} else {
+					assert.NoError(err)
+				}
+				assert.Equal(testCase.expectedValue, value)
+			}
+			r.Get(testCase.routeString, handler)
+
+			ts := httptest.NewServer(r)
+			request, _ := http.NewRequest("GET", ts.URL+testCase.queryString, nil)
+			_, _ = http.DefaultClient.Do(request)
+			ts.Close()
+			assert.True(called, "The handler was not called")
+		})
+	}
 }
