9cd48a30f56d072422debadc52e9617e07043df5
Damien Leroy <dleroy@smad.be>
2019-02-22 15:40:05 +0100

    change approach to exclude non-unit tests from tests, to something more dogmatic for Go

--- app/api/items/bdd_test.go
+++ app/api/items/bdd_test.go
@@ -1,18 +1,13 @@
+// +build !unit
+
 package items_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/France-ioi/AlgoreaBackend/testhelpers"
 )
 
 func TestMain(m *testing.M) {
-
-	if testhelpers.HasNoDBFlag() {
-		fmt.Println("Skipping BDD tests in package 'items' (no-db flag)")
-		return
-	}
-
 	testhelpers.RunGodogTests(m)
 }
