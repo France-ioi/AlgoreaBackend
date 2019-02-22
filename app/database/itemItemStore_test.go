c760c723d306bf6da999f61d47d8da7b287a5f20
Dmitry Zenovich <dzenovich@gmail.com>
2019-02-22 20:12:59 +0300

    introduce TestItemItemStore_ChildrenOf

---
+++ app/database/itemItemStore_test.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"github.com/stretchr/testify/assert"
+	"regexp"
+	"testing"
+)
+
+func TestItemItemStore_ChildrenOf(t *testing.T) {
+	db, mock := NewDBMock()
+	defer func() { _ = db.Close() }()
+
+	const parentItemID = 123
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `items_items` WHERE (items_items.idItemParent=?)")).
+		WithArgs(parentItemID).
+		WillReturnRows(mock.NewRows([]string{"ID"}))
+
+	store := NewDataStore(db).ItemItems()
+	newStore := store.ChildrenOf(parentItemID)
+	assert.NotEqual(t, store, newStore)
+
+	var result []interface{}
+	err := newStore.Scan(&result).Error()
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
