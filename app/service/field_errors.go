ae133dac7dc2695c65686cbdcd694a53ae2a6120
Dmitry Zenovich <dzenovich@gmail.com>
2019-02-27 17:40:42 +0300

    display multiple errors for incorrect form data

---
+++ app/service/field_errors.go
@@ -0,0 +1,8 @@
+package service
+
+// FieldErrors represents multiple errors for form fields
+type FieldErrors map[string][]string
+
+func (e FieldErrors) Error() string {
+	return "invalid input data"
+}
