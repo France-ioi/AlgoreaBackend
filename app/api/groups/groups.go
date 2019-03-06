695f270df0f2215f92891f26659cb95377e881a7
Dmitry Zenovich <dzenovich@gmail.com>
2019-03-06 20:40:22 +0300

    use service.MustNotBeError

--- app/api/groups/groups.go
+++ app/api/groups/groups.go
@@ -25,10 +25,9 @@ func (srv *Service) SetRoutes(router chi.Router) {
 
 func (srv *Service) checkThatUserOwnsTheGroup(user *auth.User, groupID int64) service.APIError {
 	var count int64
-	if err := srv.Store.GroupAncestors().OwnedByUser(user).
-		Where("idGroupChild = ?", groupID).Count(&count).Error(); err != nil {
-		return service.ErrUnexpected(err)
-	}
+	service.MustNotBeError(
+		srv.Store.GroupAncestors().OwnedByUser(user).
+			Where("idGroupChild = ?", groupID).Count(&count).Error())
 	if count == 0 {
 		return service.ErrForbidden(errors.New("insufficient access rights"))
 	}
