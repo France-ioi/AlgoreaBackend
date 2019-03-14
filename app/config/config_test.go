8f07d3d27823d8d78f0ef67a0727c0c89127e5b0
Dmitry Zenovich <dzenovich@gmail.com>
2019-03-14 16:06:52 +0300

    add a test for config caching + refactor tests

--- app/config/config_test.go
+++ app/config/config_test.go
@@ -12,18 +12,17 @@ import (
 func TestLoadConfig(t *testing.T) {
 	assert := assertlib.New(t)
 
+	ClearCachedConfig()
+	defer ClearCachedConfig()
+
 	// create a temp config file
 	tmpDir := os.TempDir()
-	tmpFile, err := ioutil.TempFile(tmpDir, "config-*.yaml")
+	tmpFile, err := writeConfigIntoTempFile(tmpDir, "server:\n  port: 1234\n")
 	assert.NoError(err)
-	defer func() {
-		_ = os.Remove(tmpFile.Name())
-		_ = tmpFile.Close()
-	}()
-
-	text := []byte("server:\n  port: 1234\n")
-	_, err = tmpFile.Write(text)
-	assert.NoError(err)
+	if err != nil {
+		return
+	}
+	defer func() { _ = os.Remove(tmpFile.Name()) }()
 
 	// change default config values
 	fileName := filepath.Base(tmpFile.Name())
@@ -42,4 +41,65 @@ func TestLoadConfig(t *testing.T) {
 
 	// test env
 	assert.EqualValues(999, conf.Server.WriteTimeout)
+}
+
+func TestLoadConfig_Caches(t *testing.T) {
+	assert := assertlib.New(t)
+
+	ClearCachedConfig()
+	defer ClearCachedConfig()
+
+	// create a temp config file
+	tmpDir := os.TempDir()
+	tmpFile, err := writeConfigIntoTempFile(tmpDir, "server:\n  port: 5678\n")
+	assert.NoError(err)
+	if err != nil {
+		return
+	}
+	defer func() { _ = os.Remove(tmpFile.Name()) }()
+
+	// change default config values
+	fileName := filepath.Base(tmpFile.Name())
+	configName = fileName[:len(fileName)-5] // strip the ".yaml"
+	configDir = tmpDir
+
+	conf, err := Load()
+	assert.NoError(err)
+
+	// test config override
+	assert.EqualValues(5678, conf.Server.Port)
+
+	oldConfig := loadedConfig
+
+	tmpFile, err = writeConfigIntoTempFile(tmpDir, "server:\n  port: 1234\n")
+	assert.NoError(err)
+	if err != nil {
+		return
+	}
+	defer func() { _ = os.Remove(tmpFile.Name()) }()
+
+	// change default config values
+	fileName = filepath.Base(tmpFile.Name())
+	configName = fileName[:len(fileName)-5] // strip the ".yaml"
+
+	conf, err = Load()
+	assert.NoError(err)
+	newConfig := loadedConfig
+
+	assert.EqualValues(5678, conf.Server.Port)
+	assert.Equal(oldConfig, newConfig)
+}
+
+func writeConfigIntoTempFile(dir, content string) (*os.File, error) {
+	tmpFile, err := ioutil.TempFile(dir, "config-*.yaml")
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = tmpFile.Close() }()
+
+	_, err = tmpFile.Write([]byte(content))
+	if err != nil {
+		return nil, err
+	}
+	return tmpFile, nil
 }
