faf07df545215f02d144c131ffb200b477526d77
Dmitry Zenovich <dzenovich@gmail.com>
2019-03-21 14:13:24 +0300

    add a test for ComputeAllUserItems() with cyclic graphs in items_items & items_ancestors

---
+++ app/database/user_item_store_integration_compute_all_user_items_cyclic_graph_test.go
@@ -0,0 +1,33 @@
+package database_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/France-ioi/AlgoreaBackend/app/database"
+	"github.com/France-ioi/AlgoreaBackend/testhelpers"
+)
+
+type stateResultRow struct {
+	ID                        int64  `gorm:"column:ID"`
+	AncestorsComputationState string `gorm:"column:sAncestorsComputationState"`
+}
+
+func TestUserItemStore_ComputeAllUserItems_WithCyclicGraph(t *testing.T) {
+	db := testhelpers.SetupDBWithFixture("users_items_propagation/cyclic")
+	defer func() { _ = db.Close() }()
+
+	s := database.NewDataStore(db).UserItems()
+
+	err := s.ComputeAllUserItems()
+	assert.NoError(t, err)
+	var result []stateResultRow
+	assert.NoError(t, s.Select("ID, sAncestorsComputationState").Order("ID").Scan(&result).Error())
+	assert.Equal(t, []stateResultRow{
+		{ID: 11, AncestorsComputationState: "todo"},
+		{ID: 12, AncestorsComputationState: "todo"},
+		// another user
+		{ID: 22, AncestorsComputationState: "done"},
+	}, result)
+}
