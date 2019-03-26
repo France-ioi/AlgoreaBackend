d4c7d4ad17f5c82cbe8c7b5ee2e79897b12537e9
Dmitry Zenovich <dzenovich@gmail.com>
2019-03-26 15:15:07 +0300

    format the query in the migration file

--- db/migrations/1_create_task_children_data_view.sql
+++ db/migrations/1_create_task_children_data_view.sql
@@ -11,22 +11,24 @@ CREATE ALGORITHM=UNDEFINED
   VIEW `task_children_data_view` AS
   SELECT
     `parent_users_items`.`ID` AS `idUserItem`,
-    SUM(IF(((`task_children`.`ID` IS NOT NULL) AND `task_children`.`bValidated`), 1, 0)) AS `nbChildrenValidated`,
-    SUM(IF(((`task_children`.`ID` IS NOT NULL) AND `task_children`.`bValidated`), 0, 1)) AS `nbChildrenNonValidated`,
-    SUM(IF(((`items_items`.`sCategory` = 'Validation') AND
-      (ISNULL(`task_children`.`ID`) OR (`task_children`.`bValidated` = 0))), 1, 0)) AS `nbChildrenCategory`,
+    SUM(IF(`task_children`.`ID` IS NOT NULL AND `task_children`.`bValidated`, 1, 0)) AS `nbChildrenValidated`,
+    SUM(IF(`task_children`.`ID` IS NOT NULL AND `task_children`.`bValidated`, 0, 1)) AS `nbChildrenNonValidated`,
+    SUM(IF(`items_items`.`sCategory` = 'Validation' AND
+      (ISNULL(`task_children`.`ID`) OR `task_children`.`bValidated` != 1), 1, 0)) AS `nbChildrenCategory`,
     MAX(`task_children`.`sValidationDate`) AS `maxValidationDate`,
-    MAX(IF((`items_items`.`sCategory` = 'Validation'), `task_children`.`sValidationDate`, NULL)) AS `maxValidationDateCategories`
-  FROM (
-    (
-      (`users_items` `parent_users_items`
-       JOIN `items_items` ON((`parent_users_items`.`idItem` = `items_items`.`idItemParent`)))
-      LEFT JOIN `users_items` `task_children`
-      ON(((`items_items`.`idItemChild` = `task_children`.`idItem`) AND
-        (`task_children`.`idUser` = `parent_users_items`.`idUser`)))
-    )
-    JOIN `items` ON((`items`.`ID` = `items_items`.`idItemChild`)))
-  WHERE ((`items`.`sType` <> 'Course') AND (`items`.`bNoScore` = 0))
+    MAX(IF(`items_items`.`sCategory` = 'Validation', `task_children`.`sValidationDate`, NULL)) AS `maxValidationDateCategories`
+  FROM `users_items` `parent_users_items`
+  JOIN `items_items` ON(
+    `parent_users_items`.`idItem` = `items_items`.`idItemParent`
+  )
+  LEFT JOIN `users_items` AS `task_children` ON(
+    `items_items`.`idItemChild` = `task_children`.`idItem` AND
+    `task_children`.`idUser` = `parent_users_items`.`idUser`
+  )
+  JOIN `items` ON(
+    `items`.`ID` = `items_items`.`idItemChild`
+  )
+  WHERE `items`.`sType` <> 'Course' AND `items`.`bNoScore` = 0
   GROUP BY `idUserItem`;
 SET character_set_client      = @saved_cs_client;
 SET character_set_results     = @saved_cs_results;
