5ed28c1dd6569ee6ae6c607f007fae2083469923
Dmitry Zenovich <dzenovich@gmail.com>
2019-03-27 15:19:17 +0300

    beautify queries

--- app/database/group_item_store_compute_all_access.go
+++ app/database/group_item_store_compute_all_access.go
@@ -19,89 +19,126 @@ func (s *GroupItemStore) computeAllAccess() {
 	*/
 
 	// inserting missing groups_items_propagate
-	const queryInsertMissingPropagate = "" +
-		"INSERT IGNORE INTO `groups_items_propagate` (`ID`, `sPropagateAccess`) " +
-		"SELECT `groups_items`.`ID`, 'self' as `sPropagateAccess` " +
-		"FROM `groups_items` " +
-		"WHERE `sPropagateAccess`='self' " +
-		"ON DUPLICATE KEY UPDATE `sPropagateAccess`='self'"
+	const queryInsertMissingPropagate = `
+		INSERT INTO groups_items_propagate (ID, sPropagateAccess)
+		SELECT
+			groups_items.ID,
+			'self' as sPropagateAccess
+		FROM groups_items
+		WHERE sPropagateAccess='self'
+		ON DUPLICATE KEY UPDATE sPropagateAccess='self'`
 
 	// Set groups_items as set up for propagation
 	const queryUpdatePropagateAccess = "UPDATE `groups_items` SET `sPropagateAccess`='done' WHERE `sPropagateAccess`='self'"
 
 	// inserting missing children of groups_items marked as 'children'
-	const queryInsertMissingChildren = "" +
-		"INSERT IGNORE INTO `groups_items` (`idGroup`, `idItem`, `idUserCreated`, `sCachedAccessReason`, `sAccessReason`) " +
-		"SELECT `parents`.`idGroup` as `idGroup`, `items_items`.`idItemChild` as `idItem`, " +
-		"   `parents`.`idUserCreated` as `idUserCreated`, '' as `sCachedAccessReason`, '' as `sAccessReason` " +
-		"FROM `items_items` " +
-		"JOIN `groups_items` as `parents` on `parents`.`idItem` = `items_items`.`idItemParent` " +
-		"JOIN `groups_items_propagate` as `parents_propagate` on `parents`.`ID` = `parents_propagate`.`ID` " +
-		"WHERE `parents_propagate`.`sPropagateAccess` = 'children'"
+	const queryInsertMissingChildren = `
+		INSERT IGNORE INTO groups_items (idGroup, idItem, idUserCreated, sCachedAccessReason, sAccessReason)
+		SELECT
+			parents.idGroup AS idGroup,
+			items_items.idItemChild AS idItem,
+			parents.idUserCreated AS idUserCreated,
+			'' AS sCachedAccessReason,
+			'' AS sAccessReason
+		FROM items_items
+		JOIN groups_items AS parents
+			ON parents.idItem = items_items.idItemParent
+		JOIN groups_items_propagate AS parents_propagate
+			ON parents.ID = parents_propagate.ID AND parents_propagate.sPropagateAccess = 'children'`
 
 	// mark as 'done' items that shouldn't propagate
-	const queryMarkDoNotPropagate = "" +
-		"INSERT IGNORE INTO `groups_items_propagate` (`ID`, sPropagateAccess) " +
-		"SELECT `groups_items`.`ID` as `ID`, 'done' as sPropagateAccess FROM groups_items " +
-		"JOIN `items` on `groups_items`.`idItem` = `items`.`ID` " +
-		"WHERE `items`.`bCustomChapter` = 1 ON DUPLICATE KEY UPDATE sPropagateAccess='done'"
+	const queryMarkDoNotPropagate = `
+		INSERT IGNORE INTO groups_items_propagate (ID, sPropagateAccess)
+		SELECT
+			groups_items.ID AS ID,
+			'done' as sPropagateAccess
+		FROM groups_items
+		JOIN items
+			ON groups_items.idItem = items.ID AND items.bCustomChapter
+		ON DUPLICATE KEY UPDATE sPropagateAccess='done'`
 
 	// marking 'self' groups_items sons of groups_items marked as 'children'
-	const queryMarkExistingChildren = "" +
-		"INSERT IGNORE INTO `groups_items_propagate` (`ID`, sPropagateAccess) " +
-		"SELECT `children`.`ID` as `ID`, 'self' as sPropagateAccess " +
-		"FROM `items_items` " +
-		"JOIN `groups_items` as `parents` on `parents`.`idItem` = `items_items`.`idItemParent` " +
-		"JOIN `groups_items` as `children` on `children`.`idItem` = `items_items`.`idItemChild` AND `parents`.`idGroup` = `children`.`idGroup` " +
-		"JOIN `groups_items_propagate` as `parents_propagate` on `parents`.`ID` = `parents_propagate`.`ID` " +
-		"WHERE `parents_propagate`.`sPropagateAccess` = 'children' ON DUPLICATE KEY UPDATE sPropagateAccess='self'"
+	const queryMarkExistingChildren = `
+		INSERT IGNORE INTO groups_items_propagate (ID, sPropagateAccess)
+		SELECT
+			children.ID AS ID,
+			'self' as sPropagateAccess
+		FROM items_items
+		JOIN groups_items AS parents
+			ON parents.idItem = items_items.idItemParent
+		JOIN groups_items AS children
+			ON children.idItem = items_items.idItemChild AND children.idGroup = parents.idGroup
+		JOIN groups_items_propagate AS parents_propagate
+			ON parents_propagate.ID = parents.ID AND parents_propagate.sPropagateAccess = 'children'
+		ON DUPLICATE KEY UPDATE sPropagateAccess='self'`
 
 	// marking 'children' groups_items as 'done'
-	const queryMarkFinishedItems = "" +
-		"UPDATE `groups_items_propagate`" +
-		"SET `sPropagateAccess` = 'done' " +
-		"WHERE `sPropagateAccess` = 'children'"
+	const queryMarkFinishedItems = `
+		UPDATE groups_items_propagate
+		SET sPropagateAccess = 'done'
+		WHERE sPropagateAccess = 'children'`
 
 	// computation for groups_items marked as 'self'.
 	// It also marks 'self' groups_items as 'children'
-	const queryUpdateGroupItems = "" +
-		"UPDATE `groups_items` " +
-		"LEFT JOIN ( " +
-		"  SELECT `child`.`ID`,  " +
-		"  MIN(`parent`.`sCachedFullAccessDate`) as sCachedFullAccessDate, " +
-		"  MIN(IF(`items_items`.`bAccessRestricted` = 0, `parent`.`sCachedPartialAccessDate`, NULL)) as sCachedPartialAccessDate, " +
-		"  MAX(`parent`.`bCachedManagerAccess`) as bCachedManagerAccess, " +
-		"  MIN(IF(`items_items`.`bAccessRestricted` = 1 AND `items_items`.`bAlwaysVisible` = 1, `parent`.`sCachedPartialAccessDate`, NULL)) as sCachedGrayedAccessDate, " +
-		"  MIN(`parent`.`sCachedAccessSolutionsDate`) as sCachedAccessSolutionsDate, " +
-		"  CONCAT('From ancestor group(s) ', GROUP_CONCAT(`parent`.`sAccessReason`, ', ')) AS `sAccessReasonAncestors` " +
-		"  FROM `groups_items`  `child` " +
-		"  JOIN `items_items` ON (`items_items`.`idItemChild` = `child`.`idItem`) " +
-		"  LEFT JOIN `groups_items_propagate` ON (`groups_items_propagate`.`ID` = `child`.`ID`) " +
-		"  JOIN `groups_items` `parent` ON (`items_items`.`idItemParent` = `parent`.`idItem` AND `parent`.`idGroup` = `child`.`idGroup`) " +
-		"  WHERE  " +
-		"  (`groups_items_propagate`.`sPropagateAccess` = 'self' OR `groups_items_propagate`.`ID` IS NULL) AND " +
-		"  (`parent`.`sCachedFullAccessDate` IS NOT NULL OR `parent`.`sCachedPartialAccessDate` IS NOT NULL OR `parent`.`sCachedAccessSolutionsDate` IS NOT NULL OR " +
-		"`parent`.`sFullAccessDate` IS NOT NULL  OR `parent`.`sPartialAccessDate` IS NOT NULL OR `parent`.`sAccessSolutionsDate` IS NOT NULL  OR `parent`.`bManagerAccess` != 0)   GROUP BY `child`.`ID`  " +
-		"  ) AS `new_data` " +
-		"  ON (`groups_items`.`ID` = `new_data`.ID) " +
-		"  JOIN `groups_items_propagate` ON (`groups_items_propagate`.`ID` = `groups_items`.`ID`) " +
-		"  SET " +
-		"      `groups_items`.`sCachedFullAccessDate` = LEAST(IFNULL(`new_data`.`sCachedFullAccessDate`,`groups_items`.`sFullAccessDate`), " +
-		"                                                        IFNULL(`groups_items`.`sFullAccessDate`, `new_data`.`sCachedFullAccessDate`)), " +
-		"      `groups_items`.`sCachedPartialAccessDate` = LEAST(IFNULL(`new_data`.`sCachedPartialAccessDate`,`groups_items`.`sPartialAccessDate`), " +
-		"                                                        IFNULL(`groups_items`.`sPartialAccessDate`, `new_data`.`sCachedPartialAccessDate`)), " +
-		"      `groups_items`.`bCachedManagerAccess` = GREATEST(IFNULL(`new_data`.`bCachedManagerAccess`, 0),`groups_items`.`bManagerAccess`), " +
-		"      `groups_items`.`sCachedAccessSolutionsDate` = LEAST(IFNULL(`new_data`.`sCachedAccessSolutionsDate`, `groups_items`.`sAccessSolutionsDate`), " +
-		"                                                          IFNULL(`groups_items`.`sAccessSolutionsDate`, `new_data`.`sCachedAccessSolutionsDate`)), " +
-		"      `groups_items`.`sCachedGrayedAccessDate` = `new_data`.`sCachedGrayedAccessDate`, " +
-		"      `groups_items`.`sCachedAccessReason` = `new_data`.`sAccessReasonAncestors` " +
-		"  WHERE `groups_items_propagate`.`sPropagateAccess` = 'self'"
+	const queryUpdateGroupItems = `
+		UPDATE groups_items
+		LEFT JOIN (
+			SELECT
+				child.ID,
+				MIN(parent.sCachedFullAccessDate) AS sCachedFullAccessDate,
+				MIN(IF(items_items.bAccessRestricted = 0, parent.sCachedPartialAccessDate, NULL)) AS sCachedPartialAccessDate,
+				MAX(parent.bCachedManagerAccess) AS bCachedManagerAccess,
+				MIN(IF(items_items.bAccessRestricted AND items_items.bAlwaysVisible, parent.sCachedPartialAccessDate, NULL)) AS sCachedGrayedAccessDate,
+				MIN(parent.sCachedAccessSolutionsDate) AS sCachedAccessSolutionsDate,
+				CONCAT('From ancestor group(s) ', GROUP_CONCAT(parent.sAccessReason, ', ')) AS sAccessReasonAncestors
+			FROM groups_items AS child
+			JOIN items_items
+				ON items_items.idItemChild = child.idItem
+			LEFT JOIN groups_items_propagate
+				ON groups_items_propagate.ID = child.ID
+			JOIN groups_items AS parent
+				ON parent.idItem = items_items.idItemParent AND parent.idGroup = child.idGroup
+			WHERE
+				(groups_items_propagate.sPropagateAccess = 'self' OR groups_items_propagate.ID IS NULL) AND
+				(
+					parent.sCachedFullAccessDate IS NOT NULL OR
+					parent.sCachedPartialAccessDate IS NOT NULL OR
+					parent.sCachedAccessSolutionsDate IS NOT NULL OR
+					parent.sFullAccessDate IS NOT NULL OR
+					parent.sPartialAccessDate IS NOT NULL OR
+					parent.sAccessSolutionsDate IS NOT NULL OR
+					parent.bManagerAccess
+				)
+			GROUP BY child.ID
+		) AS new_data
+			USING(ID)
+		JOIN groups_items_propagate USING(ID)
+		SET
+			groups_items.sCachedFullAccessDate = LEAST(
+				IFNULL(new_data.sCachedFullAccessDate, groups_items.sFullAccessDate),
+				IFNULL(groups_items.sFullAccessDate, new_data.sCachedFullAccessDate)
+			),
+			groups_items.sCachedPartialAccessDate = LEAST(
+				IFNULL(new_data.sCachedPartialAccessDate, groups_items.sPartialAccessDate),
+				IFNULL(groups_items.sPartialAccessDate, new_data.sCachedPartialAccessDate)
+			),
+			groups_items.bCachedManagerAccess = GREATEST(
+				IFNULL(new_data.bCachedManagerAccess, 0),
+				groups_items.bManagerAccess
+			),
+			groups_items.sCachedAccessSolutionsDate = LEAST(
+				IFNULL(new_data.sCachedAccessSolutionsDate, groups_items.sAccessSolutionsDate),
+				IFNULL(groups_items.sAccessSolutionsDate, new_data.sCachedAccessSolutionsDate)
+			),
+			groups_items.sCachedGrayedAccessDate = new_data.sCachedGrayedAccessDate,
+			groups_items.sCachedAccessReason = new_data.sAccessReasonAncestors
+		WHERE groups_items_propagate.sPropagateAccess = 'self'`
 
 	// marking 'self' groups_items as 'children'
-	const queryMarkChildrenItems = "" +
-		"UPDATE `groups_items_propagate`" +
-		"SET `sPropagateAccess` = 'children' " +
-		"WHERE `sPropagateAccess` = 'self'"
+	const queryMarkChildrenItems = `
+		UPDATE groups_items_propagate
+		SET sPropagateAccess = 'children'
+		WHERE sPropagateAccess = 'self'`
 
 	hasChanges := true
 	for hasChanges {
