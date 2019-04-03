445b8ef3de1158ea3272f4ab277eea83bdf86676
Dmitry Zenovich <dzenovich@gmail.com>
2019-04-03 20:18:30 +0300

    add TestBase_GetUser

--- app/service/common_test.go
+++ app/service/common_test.go
@@ -7,11 +7,35 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/France-ioi/AlgoreaBackend/app/auth"
 	"github.com/France-ioi/AlgoreaBackend/app/database"
 )
+
+func TestBase_GetUser(t *testing.T) {
+	db, mock := database.NewDBMock()
+	mock.ExpectQuery("").WithArgs(42).
+		WillReturnRows(sqlmock.NewRows([]string{"idGroupOwned"}).AddRow(int64(2)))
+	middleware := auth.MockUserIDMiddleware(42)
+	called := false
+	ts := httptest.NewServer(middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		called = true
+		srv := &Base{Store: database.NewDataStore(db)}
+		user := srv.GetUser(r)
+		assert.Equal(t, int64(42), user.UserID)
+		assert.Equal(t, int64(2), user.OwnedGroupID())
+	})))
+	defer ts.Close()
+
+	request, _ := http.NewRequest("GET", ts.URL, nil)
+	_, err := http.DefaultClient.Do(request)
+	assert.NoError(t, err)
+
+	assert.True(t, called)
+}
 
 func TestBase_SetQueryLimit(t *testing.T) {
 	testCases := []struct {
