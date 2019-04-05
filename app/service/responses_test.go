45fc813105310a2c87995b56b0b4cc494b57261a
Dmitry Zenovich <dzenovich@gmail.com>
2019-04-05 13:07:14 +0300

    add TestResponse_Render

--- app/service/responses_test.go
+++ app/service/responses_test.go
@@ -34,3 +34,10 @@ func TestCreationSuccess(t *testing.T) {
 	assert.Equal(`{"success":true,"message":"created","data":{"ID":42}}`+"\n", recorder.Body.String())
 	assert.Equal(http.StatusCreated, recorder.Code)
 }
+
+func TestResponse_Render(t *testing.T) {
+	response := &Response{HTTPStatusCode: http.StatusOK, Message: "", Success: true}
+	recorder := httpResponseForResponse(response)
+	assertlib.Equal(t, `{"success":true,"message":"success"}`+"\n", recorder.Body.String())
+	assertlib.Equal(t, http.StatusOK, recorder.Code)
+}
