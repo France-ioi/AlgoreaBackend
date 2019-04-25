6c39cf8b8ea7c318d184c30db9333ae384076070
Dmitry Zenovich <dzenovich@gmail.com>
2019-04-25 19:35:56 +0300

    implement user/memberships

---
+++ app/api/user/get_memberships.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+
+	"github.com/France-ioi/AlgoreaBackend/app/database"
+	"github.com/France-ioi/AlgoreaBackend/app/service"
+)
+
+func (srv *Service) getMemberships(w http.ResponseWriter, r *http.Request) service.APIError {
+	user := srv.GetUser(r)
+	err := user.Load()
+	if err == database.ErrUserNotFound {
+		return service.InsufficientAccessRightsError
+	}
+	service.MustNotBeError(err)
+
+	selfGroupID, _ := user.SelfGroupID()
+	query := srv.Store.GroupGroups().
+		Select(`
+			groups_groups.ID,
+			groups_groups.sStatusDate,
+			groups_groups.sType,
+			groups.ID AS group__ID,
+			groups.sName AS group__sName,
+			groups.sDescription AS group__sDescription,
+			groups.sType AS group__sType`).
+		Joins("JOIN groups ON groups.ID = groups_groups.idGroupParent").
+		Where("groups_groups.sType IN ('invitationAccepted', 'requestAccepted', 'direct')").
+		Where("groups_groups.idGroupChild = ?", selfGroupID)
+
+	query = service.SetQueryLimit(r, query)
+	query, apiError := service.ApplySortingAndPaging(r, query,
+		map[string]*service.FieldSortingParams{
+			"status_date": {ColumnName: "groups_groups.sStatusDate", FieldType: "time"},
+			"id":          {ColumnName: "groups_groups.ID", FieldType: "int64"}},
+		"-status_date")
+	if apiError != service.NoError {
+		return apiError
+	}
+
+	var result []map[string]interface{}
+	service.MustNotBeError(query.ScanIntoSliceOfMaps(&result).Error())
+	convertedResult := service.ConvertSliceOfMapsFromDBToJSON(result)
+
+	render.Respond(w, r, convertedResult)
+	return service.NoError
+}
---
+++ app/api/user/get_memberships.robustness.feature
@@ -0,0 +1,18 @@
+Feature: Get group memberships for the current user - robustness
+  Background:
+    Given the database has the following table 'users':
+      | ID | sLogin | tempUser | idGroupSelf | idGroupOwned | sFirstName  | sLastName | iGrade |
+      | 1  | owner  | 0        | 21          | 22           | Jean-Michel | Blanquer  | 3      |
+
+  Scenario: User doesn't exist
+    Given I am the user with ID "4"
+    When I send a GET request to "/user/memberships"
+    Then the response code should be 403
+    And the response error message should contain "Insufficient access rights"
+
+  Scenario: sort is incorrect
+    Given I am the user with ID "1"
+    When I send a GET request to "/user/memberships?sort=myname"
+    Then the response code should be 400
+    And the response error message should contain "Unallowed field in sorting parameters: "myname""
+
