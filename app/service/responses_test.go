8df15a5eb87229cc184a92c4591a0039a0fd0e11
Dmitry Zenovich <dzenovich@gmail.com>
2019-05-24 20:45:23 +0300

    introduce service.UpdateSuccess(), add a test

--- app/service/responses_test.go
+++ app/service/responses_test.go
@@ -35,6 +35,18 @@ func TestCreationSuccess(t *testing.T) {
 	assert.Equal(http.StatusCreated, recorder.Code)
 }
 
+func TestUpdateSuccess(t *testing.T) {
+	assert := assertlib.New(t)
+
+	data := struct {
+		Info string `json:"info"`
+	}{"some info"}
+
+	recorder := httpResponseForResponse(UpdateSuccess(data))
+	assert.Equal(`{"success":true,"message":"updated","data":{"info":"some info"}}`+"\n", recorder.Body.String())
+	assert.Equal(http.StatusOK, recorder.Code)
+}
+
 func TestDeletionSuccess(t *testing.T) {
 	assert := assertlib.New(t)
 
