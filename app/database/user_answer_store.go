e5ba2384b4e18c74bd258e61d1d9dfbd4a1bcf71
Dmitry Zenovich <dzenovich@gmail.com>
2019-06-03 13:58:33 +0300

    introduce UserAnswerStore.GetOrCreateCurrentAnswer()

--- app/database/user_answer_store.go
+++ app/database/user_answer_store.go
@@ -1,5 +1,7 @@
 package database
 
+import "github.com/jinzhu/gorm"
+
 // UserAnswerStore implements database operations on `users_answers`
 type UserAnswerStore struct {
 	*DataStore
@@ -44,5 +46,35 @@ func (s *UserAnswerStore) SubmitNewAnswer(userID, itemID, attemptID int64, answe
 				VALUES (?, ?, ?, ?, ?, NOW(), 0)`,
 			userAnswerID, userID, itemID, attemptID, answer).Error
 	})
+	return userAnswerID, err
+}
+
+// GetOrCreateCurrentAnswer returns an ID of the current users_answers for given userID, itemID, attemptID
+// or inserts a new row with sType='Current' and sSubmissionDate=NOW() into the `users_answers` table.
+func (s *UserAnswerStore) GetOrCreateCurrentAnswer(userID, itemID int64, attemptID *int64) (userAnswerID int64, err error) {
+	s.mustBeInTransaction()
+	recoverPanics(&err)
+
+	query := s.WithWriteLock().
+		Where("idUser = ?", userID).
+		Where("idItem = ?", itemID).
+		Where("sType = 'Current'")
+	if attemptID == nil {
+		query = query.Where("idAttempt IS NULL")
+	} else {
+		query = query.Where("idAttempt = ?", *attemptID)
+	}
+	err = query.PluckFirst("ID", &userAnswerID).Error()
+	if gorm.IsRecordNotFoundError(err) {
+		err = s.retryOnDuplicatePrimaryKeyError(func(db *DB) error {
+			store := NewDataStore(db)
+			userAnswerID = store.NewID()
+			return db.Exec(`
+				INSERT INTO users_answers (ID, idUser, idItem, idAttempt, sType, sSubmissionDate)
+				VALUES (?, ?, ?, ?, 'Current', NOW())`,
+				userAnswerID, userID, itemID, attemptID).Error()
+		})
+	}
+	mustNotBeError(err)
 	return userAnswerID, err
 }
