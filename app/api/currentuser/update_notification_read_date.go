2ca90a14ee3cba89491678c20008bb5209976e1d
Dmitry Zenovich <dzenovich@gmail.com>
2019-06-18 15:08:09 +0300

    implement userNotificationReadDateUpdate service

---
+++ app/api/currentuser/update_notification_read_date.go
@@ -0,0 +1,41 @@
+package currentuser
+
+// swagger:operation PUT /current-user/notification-read-date userNotificationReadDateUpdate
+// ---
+// summary: Update user's notification read date
+// description: Set users.sNotificationReadDate to NOW() for the current user
+// responses:
+//   "200":
+//     "$ref": "#/responses/updatedResponse"
+//   "401":
+//     "$ref": "#/responses/unauthorizedResponse"
+//   "403":
+//     "$ref": "#/responses/forbiddenResponse"
+//   "500":
+//     "$ref": "#/responses/internalErrorResponse"
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+	"github.com/jinzhu/gorm"
+
+	"github.com/France-ioi/AlgoreaBackend/app/database"
+	"github.com/France-ioi/AlgoreaBackend/app/service"
+)
+
+func (srv *Service) updateNotificationReadDate(w http.ResponseWriter, r *http.Request) service.APIError {
+	user := srv.GetUser(r)
+	err := user.Load()
+	if err == database.ErrUserNotFound {
+		return service.InsufficientAccessRightsError
+	}
+	service.MustNotBeError(err)
+	service.MustNotBeError(srv.Store.Users().ByID(user.UserID).
+		UpdateColumn("sNotificationReadDate", gorm.Expr("NOW()")).Error())
+
+	response := service.Response{Success: true, Message: "updated"}
+	render.Respond(w, r, &response)
+
+	return service.NoError
+}
