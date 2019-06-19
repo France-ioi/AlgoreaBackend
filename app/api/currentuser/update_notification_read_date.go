6bbb00543fc48f49f3dbea620d98c83b9be3ab7c
Dmitry Zenovich <dzenovich@gmail.com>
2019-06-19 16:47:41 +0300

    do not load user data from the DB in updateNotificationReadDate()

--- app/api/currentuser/update_notification_read_date.go
+++ app/api/currentuser/update_notification_read_date.go
@@ -6,7 +6,6 @@ import (
 	"github.com/go-chi/render"
 	"github.com/jinzhu/gorm"
 
-	"github.com/France-ioi/AlgoreaBackend/app/database"
 	"github.com/France-ioi/AlgoreaBackend/app/service"
 )
 
@@ -19,17 +18,10 @@ import (
 //     "$ref": "#/responses/updatedResponse"
 //   "401":
 //     "$ref": "#/responses/unauthorizedResponse"
-//   "403":
-//     "$ref": "#/responses/forbiddenResponse"
 //   "500":
 //     "$ref": "#/responses/internalErrorResponse"
 func (srv *Service) updateNotificationReadDate(w http.ResponseWriter, r *http.Request) service.APIError {
 	user := srv.GetUser(r)
-	err := user.Load()
-	if err == database.ErrUserNotFound {
-		return service.InsufficientAccessRightsError
-	}
-	service.MustNotBeError(err)
 	service.MustNotBeError(srv.Store.Users().ByID(user.UserID).
 		UpdateColumn("sNotificationReadDate", gorm.Expr("NOW()")).Error())
 
