56a8fbcd8568d1b8af42ef0564d5246ae8f62ea5
Dmitry Zenovich <dzenovich@gmail.com>
2019-06-28 21:04:30 +0300

    eliminate data races in Server.Start()

--- app/server.go
+++ app/server.go
@@ -31,11 +31,9 @@ func NewServer(app *Application) (*Server, error) {
 
 // Start runs ListenAndServe on the http.Server with graceful shutdown.
 func (srv *Server) Start() {
-	var err error
-
 	log.Println("Starting server...")
 	go func() {
-		if err = srv.ListenAndServe(); err != http.ErrServerClosed {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 			panic(err)
 		}
 	}()
@@ -46,7 +44,7 @@ func (srv *Server) Start() {
 	signal.Notify(quit, os.Interrupt)
 	sig := <-quit
 	log.Println("Shutting down server... Reason:", sig)
-	if err = srv.Shutdown(context.Background()); err != nil {
+	if err := srv.Shutdown(context.Background()); err != nil {
 		panic(err)
 	}
 	log.Println("Server gracefully stopped")
