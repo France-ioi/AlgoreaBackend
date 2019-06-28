529b5e383ab25e55b9df784b9bb61ab5154b383e
Dmitry Zenovich <dzenovich@gmail.com>
2019-06-28 20:59:34 +0300

    stub auth proxy for "dev" environment

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 
 	"github.com/spf13/cobra"
 
@@ -26,6 +30,20 @@ func init() { // nolint:gochecknoinits
 			application, err = app.New(environment)
 			if err != nil {
 				log.Fatal(err)
+			}
+
+			if environment == "dev" {
+				backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.Header().Set("Content-Type", "application/json")
+					w.WriteHeader(http.StatusOK)
+					dataJSON := fmt.Sprintf(`{"userID": %d, "error":""}`, 1) // user_id = 1
+					_, _ = w.Write([]byte(dataJSON))                         // nolint
+				}))
+				defer backend.Close()
+
+				// put the backend URL into the config
+				backendURL, _ := url.Parse(backend.URL) // nolint
+				application.Config.Auth.ProxyURL = backendURL.String()
 			}
 
 			var server *app.Server
