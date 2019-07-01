a9fdc29935878d2c11a5ce740834c23cf61748ef
Dmitry Zenovich <dzenovich@gmail.com>
2019-07-01 21:45:10 +0300

    modify index [INDEX IDType (ID, sType)] -> [INDEX `sType` (`sType`)]

--- db/migrations/5_add_index_IDType_on_groups.sql
+++ db/migrations/5_add_index_IDType_on_groups.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
-ALTER TABLE `groups` ADD INDEX IDType (ID, sType);
+ALTER TABLE `groups` ADD INDEX `sType` (`sType`);
 
 
 -- +migrate Down
-ALTER TABLE `groups` DROP INDEX IDType;
+ALTER TABLE `groups` DROP INDEX `sType`;
 
