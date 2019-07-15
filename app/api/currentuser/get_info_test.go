55b03dbf7aecb3d50acd0a6a49889c4f4eeab1df
Dmitry Zenovich <dzenovich@gmail.com>
2019-07-15 18:37:25 +0300

    add a test that checks the case when the user has been deleted right between the user middleware and the currentuser/get_info service

---
+++ app/api/currentuser/get_info_test.go
@@ -0,0 +1,28 @@
+package currentuser
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/France-ioi/AlgoreaBackend/app/database"
+	"github.com/France-ioi/AlgoreaBackend/app/service"
+)
+
+func TestService_getInfo_Returns403WhenUserNotFound(t *testing.T) {
+	db, mock := database.NewDBMock()
+	defer func() { _ = db.Close() }()
+	mock.ExpectQuery("^SELECT").WillReturnRows(mock.NewRows([]string{"ID"})) // no rows
+
+	srv := &Service{Base: service.Base{Store: database.NewDataStore(db)}}
+	monkey.PatchInstanceMethod(reflect.TypeOf(&srv.Base), "GetUser", func(*service.Base, *http.Request) *database.User {
+		return &database.User{ID: 123}
+	})
+	result := srv.getInfo(nil, nil)
+	assert.Equal(t, service.InsufficientAccessRightsError, result)
+
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
