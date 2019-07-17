eae020ce0d8a547419cce0c48a3f33880929fba4
Dmitry Zenovich <dzenovich@gmail.com>
2019-07-17 05:45:05 +0300

    add new DB table: login_states

---
+++ app/database/login_state_store.go
@@ -0,0 +1,11 @@
+package database
+
+// LoginStateStore implements database operations on login_states
+type LoginStateStore struct {
+	*DataStore
+}
+
+// DeleteExpired deletes all expired cookie/state pairs from the DB
+func (s *LoginStateStore) DeleteExpired() error {
+	return s.Delete("sExpirationDate <= NOW()").Error()
+}
---
+++ app/database/login_state_store_test.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"errors"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoginStateStore_DeleteExpired(t *testing.T) {
+	db, mock := NewDBMock()
+	expectedError := errors.New("some error")
+	mock.ExpectExec("^" + regexp.QuoteMeta("DELETE FROM `login_states` WHERE (sExpirationDate <= NOW())")).
+		WillReturnError(expectedError)
+
+	err := NewDataStore(db).LoginStates().DeleteExpired()
+
+	assert.Equal(t, expectedError, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
