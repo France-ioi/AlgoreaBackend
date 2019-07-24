1b1d1d59df82aa9a87193ddfa3d5df631f955422
Dmitry Zenovich <dzenovich@gmail.com>
2019-07-25 01:54:44 +0300

    fix formatting

--- db/migrations/8_create_table_sessions.sql
+++ db/migrations/8_create_table_sessions.sql
@@ -1,12 +1,12 @@
 -- +migrate Up
 CREATE TABLE `sessions` (
-    `sAccessToken` VARBINARY(64) NOT NULL,
-    `idUser` BIGINT(20) NOT NULL,
-    `sExpirationDate` DATETIME NOT NULL,
-    `sIssuedAtDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `sIssuer` ENUM('backend', 'login-module'),
-    PRIMARY KEY (`sAccessToken`),
-    KEY `sExpirationDate` (`sExpirationDate`)
+  `sAccessToken` VARBINARY(64) NOT NULL,
+  `idUser` BIGINT(20) NOT NULL,
+  `sExpirationDate` DATETIME NOT NULL,
+  `sIssuedAtDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sIssuer` ENUM('backend', 'login-module'),
+  PRIMARY KEY (`sAccessToken`),
+  KEY `sExpirationDate` (`sExpirationDate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- +migrate Down
--- db/migrations/9_create_table_login_states.sql
+++ db/migrations/9_create_table_login_states.sql
@@ -1,10 +1,10 @@
 -- +migrate Up
 CREATE TABLE `login_states` (
-    `sCookie` BINARY(32) NOT NULL,
-    `sState` BINARY(32) NOT NULL,
-    `sExpirationDate` DATETIME NOT NULL,
-    PRIMARY KEY (`sCookie`),
-    KEY `sExpirationDate` (`sExpirationDate`)
+  `sCookie` BINARY(32) NOT NULL,
+  `sState` BINARY(32) NOT NULL,
+  `sExpirationDate` DATETIME NOT NULL,
+  PRIMARY KEY (`sCookie`),
+  KEY `sExpirationDate` (`sExpirationDate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- +migrate Down
