c3950d91ee2c3bbe02dcbb3c575a2e67e433457c
Dmitry Zenovich <dzenovich@gmail.com>
2019-08-14 17:34:43 +0300

    add swagger specs for groupsMembershipHistory

--- app/doc/models.go
+++ app/doc/models.go
@@ -1,5 +1,7 @@
 package doc
 
+import "time"
+
 // swagger:model userCreateTmpResponse
 type userCreateTmpResponse struct {
 	// description
@@ -14,4 +16,27 @@ type userCreateTmpResponse struct {
 		// required:true
 		ExpiresIn int32 `json:"expires_in"`
 	} `json:"data"`
+}
+
+// swagger:model groupsMembershipHistoryResponseRow
+type groupsMembershipHistoryResponseRow struct {
+	// `groups_groups.ID`
+	// required: true
+	ID int64 `json:"id"`
+	// `groups_groups.sStatusDate`
+	// required: true
+	StatusDate time.Time `json:"status_date"`
+	// `groups_groups.sType`
+	// required: true
+	// enum: invitationSent,requestSent,invitationAccepted,requestAccepted,invitationRefused,requestRefused,removed,left
+	Type string `json:"type"`
+
+	// required: true
+	Group struct {
+		// required: true
+		Name string `json:"name"`
+		// required: true
+		// enum: Class,Team,Club,Friends,Other
+		Type string `json:"type"`
+	} `json:"group"`
 }
