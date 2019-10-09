9c48ae26e4a6ec5123091a163385c5710cd99b40
Dmitry Zenovich <dzenovich@gmail.com>
2019-10-09 05:06:31 +0300

    fix undo of db/migrations/55_drop_column_groups_items_additional_time.sql

--- db/migrations/55_drop_column_groups_items_additional_time.sql
+++ db/migrations/55_drop_column_groups_items_additional_time.sql
@@ -7,8 +7,8 @@ ALTER TABLE `groups_items`
         COMMENT 'Time that was attributed (can be negative) to this group for this item (typically, for  time-limited items)'
         AFTER `cached_manager_access`;
 
-INSERT INTO `groups_items` (`group_id`, `item_id`, `additional_time`)
-    SELECT group_id, groups_contest_items.item_id, groups_contest_items.additional_time
+INSERT INTO `groups_items` (`group_id`, `item_id`, `additional_time`,`creator_user_id`)
+    SELECT group_id, groups_contest_items.item_id, groups_contest_items.additional_time, 0
     FROM groups_contest_items
     WHERE groups_contest_items.additional_time != '00:00:00'
 ON DUPLICATE KEY UPDATE additional_time = groups_contest_items.additional_time;
