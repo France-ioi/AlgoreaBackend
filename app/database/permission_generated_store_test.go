93d96d500f8b81ff8a72ca154a28b5fa6afcb646
Dmitry Zenovich <dzenovich@gmail.com>
2019-11-12 04:59:18 +0300

    add more tests

--- app/database/permission_generated_store_test.go
+++ app/database/permission_generated_store_test.go
@@ -31,3 +31,28 @@ func TestPermissionGeneratedStore_AccessRightsForItemsVisibleToUser(t *testing.T
 	assert.NoError(t, err)
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
+
+func TestPermissionGeneratedStore_WithViewPermissionForUser(t *testing.T) {
+	db, mock := NewDBMock()
+	defer func() { _ = db.Close() }()
+
+	mockUser := &User{GroupID: 2, OwnedGroupID: ptrInt64(3), DefaultLanguageID: 4}
+
+	mockPermissionEnumQueries(mock)
+	defer clearAllPermissionEnums()
+
+	mock.ExpectQuery("^"+regexp.QuoteMeta(
+		"SELECT item_id, MAX(can_view_generated_value) AS can_view_generated_value "+
+			"FROM `permissions_generated` JOIN "+
+			"( SELECT * FROM groups_ancestors_active "+
+			"WHERE groups_ancestors_active.child_group_id = ? ) AS ancestors "+
+			"ON ancestors.ancestor_group_id = permissions_generated.group_id "+
+			"WHERE (can_view_generated_value >= ?) GROUP BY permissions_generated.item_id")+"$").
+		WithArgs(2, NewDataStore(db).PermissionsGranted().ViewIndexByName("content")).
+		WillReturnRows(mock.NewRows([]string{"id"}))
+
+	var result []interface{}
+	err := NewDataStore(db).PermissionsGenerated().WithViewPermissionForUser(mockUser, "content").Scan(&result).Error()
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
