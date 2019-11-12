9d508530307cd8ab9a8758bf2fc370a79477237f
Dmitry Zenovich <dzenovich@gmail.com>
2019-11-12 17:12:35 +0300

    do not require is_owner to be 0 in PermissionGrantedStore.removeContentAccess()

--- app/database/permission_granted_store.go
+++ app/database/permission_granted_store.go
@@ -33,7 +33,6 @@ func (s *PermissionGrantedStore) After() (err error) {
 func (s *PermissionGrantedStore) removeContentAccess(groupID, itemID int64) {
 	mustNotBeError(s.Where("item_id = ?", itemID).Where("group_id = ?", groupID).
 		Where("giver_group_id = -1").Where("can_view = 'content'").
-		Where("is_owner = 0").
 		UpdateColumn("can_view", "none").Error())
 }
 
