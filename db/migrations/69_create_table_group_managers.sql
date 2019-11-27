973bdcbf9db2d8c0e9ab6b386ef9d790ddc455fb
Dmitry Zenovich <dzenovich@gmail.com>
2019-11-27 07:27:15 +0300

    set default values for columns of the group_managers table

--- db/migrations/69_create_table_group_managers.sql
+++ db/migrations/69_create_table_group_managers.sql
@@ -2,9 +2,9 @@
 CREATE TABLE `group_managers` (
     `group_id` BIGINT(20) NOT NULL,
     `manager_id` BIGINT(20) NOT NULL,
-    `can_manage` ENUM('none', 'memberships', 'memberships_and_group') NOT NULL,
-    `can_grant_group_access` TINYINT(1) NOT NULL,
-    `can_watch_members` TINYINT(1) NOT NULL,
+    `can_manage` ENUM('none', 'memberships', 'memberships_and_group') NOT NULL DEFAULT 'none',
+    `can_grant_group_access` TINYINT(1) NOT NULL DEFAULT 0,
+    `can_watch_members` TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`group_id`, `manager_id`),
     CONSTRAINT `fk_group_managers_group_id_groups_id` FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`) ON DELETE CASCADE,
     CONSTRAINT `fk_group_managers_manager_id_groups_id` FOREIGN KEY (`manager_id`) REFERENCES `groups`(`id`) ON DELETE CASCADE
