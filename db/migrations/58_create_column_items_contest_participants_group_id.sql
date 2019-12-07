189a7fecd2a10fb6886f6eeddc84a8fe956c4a39
Dmitry Zenovich <dzenovich@gmail.com>
2019-12-07 07:41:16 +0300

    update comment of items.contest_participants_group_id

--- db/migrations/58_create_column_items_contest_participants_group_id.sql
+++ db/migrations/58_create_column_items_contest_participants_group_id.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE `items` ADD COLUMN `contest_participants_group_id` bigint(20) DEFAULT NULL
-    COMMENT 'id of the group to which all the contest participants (users or teams) belong.'
+    COMMENT 'Group to which all the contest participants (users or teams) belong. Must not be null for a contest.'
     AFTER `qualified_group_id`;
 
 -- +migrate Down
