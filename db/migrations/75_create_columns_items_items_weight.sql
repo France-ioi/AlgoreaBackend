f87aac91999688d2605cc151e4e0954c975f9354
Dmitry Zenovich <dzenovich@gmail.com>
2019-12-09 20:07:17 +0300

    make items_items.score_weight UNSIGNED

--- db/migrations/75_create_columns_items_items_weight.sql
+++ db/migrations/75_create_columns_items_items_weight.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE `items_items`
-    ADD COLUMN `score_weight` TINYINT NOT NULL DEFAULT '1' COMMENT 'Weight of this child in his parent\'s score computation'
+    ADD COLUMN `score_weight` TINYINT UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Weight of this child in his parent\'s score computation'
         AFTER `difficulty`;
 
 -- +migrate Down
