52a06a6c61b9f85469f982f79de162382dc01e55
Dmitry Zenovich <dzenovich@gmail.com>
2019-12-09 18:37:28 +0300

    change column items_items.weight FLOAT -> items_items.score_weight TINYINT

--- db/migrations/75_create_columns_items_items_weight.sql
+++ db/migrations/75_create_columns_items_items_weight.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 ALTER TABLE `items_items`
-    ADD COLUMN `weight` FLOAT NOT NULL DEFAULT '1' COMMENT 'Weight of this child in his parent\'s score computation'
+    ADD COLUMN `score_weight` TINYINT NOT NULL DEFAULT '1' COMMENT 'Weight of this child in his parent\'s score computation'
         AFTER `difficulty`;
 
 -- +migrate Down
-ALTER TABLE `items_items` DROP COLUMN `weight`;
+ALTER TABLE `items_items` DROP COLUMN `score_weight`;
