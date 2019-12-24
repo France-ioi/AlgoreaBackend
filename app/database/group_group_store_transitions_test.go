9ed9f4e013ae7a130fd6293dc45513023cab5a74
Dmitry Zenovich <dzenovich@gmail.com>
2019-12-24 22:07:00 +0300

    add TestGroupApprovals_ToArray

--- app/database/group_group_store_transitions_test.go
+++ app/database/group_group_store_transitions_test.go
@@ -86,3 +86,40 @@ func TestGroupApprovals_FromString(t *testing.T) {
 		})
 	}
 }
+
+func TestGroupApprovals_ToArray(t *testing.T) {
+	for _, test := range []struct {
+		name           string
+		groupApprovals GroupApprovals
+		expectedArray  []string
+	}{
+		{
+			name: "all are set",
+			groupApprovals: GroupApprovals{
+				PersonalInfoViewApproval: true, LockMembershipApproval: true, WatchApproval: true,
+			},
+			expectedArray: []string{"personal_info_view", "lock_membership", "watch"},
+		},
+		{name: "none are set", expectedArray: []string{}},
+		{
+			name:           "personal_info_view",
+			groupApprovals: GroupApprovals{PersonalInfoViewApproval: true},
+			expectedArray:  []string{"personal_info_view"},
+		},
+		{
+			name:           "lock_membership",
+			groupApprovals: GroupApprovals{LockMembershipApproval: true},
+			expectedArray:  []string{"lock_membership"},
+		},
+		{
+			name:           "watch",
+			groupApprovals: GroupApprovals{WatchApproval: true},
+			expectedArray:  []string{"watch"},
+		},
+	} {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expectedArray, test.groupApprovals.ToArray())
+		})
+	}
+}
