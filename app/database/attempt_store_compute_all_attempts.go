653aadf586c800470490def01f08145d6ba356b2
Dmitry Zenovich <dzenovich@gmail.com>
2020-01-29 19:52:02 +0300

    add more comments in AttemptStore.ComputeAllAttempts()

--- app/database/attempt_store_compute_all_attempts.go
+++ app/database/attempt_store_compute_all_attempts.go
@@ -232,8 +232,11 @@ func (s *AttemptStore) ComputeAllAttempts() (err error) {
 
 	// If items have been unlocked, need to recompute access
 	if groupsUnlocked > 0 {
+		// generate permissions_generated from permissions_granted
 		mustNotBeError(s.PermissionsGranted().After())
-		return s.ComputeAllAttempts() // we should compute attempts again as new permissions were set
+		// we should compute attempts again as new permissions were set and
+		// triggers on permissions_generated likely marked some attempts as 'changed'
+		return s.ComputeAllAttempts()
 	}
 	return nil
 }
