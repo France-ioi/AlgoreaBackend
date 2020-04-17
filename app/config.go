1f65188383f44b8c4f4dcfc97246f45ea0be1b35
Damien Leroy <dleroy@smad.be>
2020-04-17 14:33:08 +0200

    config: add tests / helper functions

--- app/config.go
+++ app/config.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"path/filepath"
 	"runtime"
@@ -14,11 +15,9 @@ import (
 	"github.com/France-ioi/AlgoreaBackend/app/token"
 )
 
-const defaultConfigName = "config"
-
-var (
-	configName = defaultConfigName
-	configDir  = configDirectory()
+const (
+	defaultConfigName = "config"
+	envPrefix         = "algorea"
 )
 
 // Configurations keys are sub configuration that can be fetched
@@ -31,7 +30,7 @@ const (
 	domainsConfigKey  string = "domains"
 )
 
-// LoadConfig loads and return the app configuration from files, flags, env, ...
+// LoadConfig loads and return the global configuration from files, flags, env, ...
 // The precedence of config values in viper is the following:
 // 1) explicit call to Set
 // 2) flag (i.e., settable by command line)
@@ -40,18 +39,22 @@ const (
 // 5) key/value store
 // 6) default
 func LoadConfig() *viper.Viper {
+	return loadConfigFrom(defaultConfigName, configDirectory())
+}
+
+func loadConfigFrom(filename, directory string) *viper.Viper {
 	var err error
 
 	config := viper.New()
 
 	// through env variables
-	config.SetEnvPrefix("algorea")                           // env variables must be prefixed by "ALGOREA_"
+	config.SetEnvPrefix(envPrefix)                           // env variables must be prefixed by "ALGOREA_"
 	config.SetEnvKeyReplacer(strings.NewReplacer(".", "__")) // substructs use "__" as key separator
 	config.AutomaticEnv()                                    // read in environment variables
 
 	// through the config file
-	config.SetConfigName(configName)
-	config.AddConfigPath(configDir)
+	config.SetConfigName(filename)
+	config.AddConfigPath(directory)
 
 	if err = config.ReadInConfig(); err != nil {
 		log.Print("Cannot read the main config file, ignoring it: ", err)
@@ -60,18 +63,10 @@ func LoadConfig() *viper.Viper {
 	environment := appenv.Env()
 	log.Printf("Loading environment: %s\n", environment)
 
-	config.SetConfigName(configName + "." + environment)
+	config.SetConfigName(filename + "." + environment)
 	if err = config.MergeInConfig(); err != nil {
 		log.Printf("Cannot merge %q config file, ignoring it: %s", environment, err)
 	}
-
-	// Set empty map as default value to expected keys so that the subconfiguration is empty and not null
-	config.SetDefault(serverConfigKey, map[string]interface{}{})
-	config.SetDefault(databaseConfigKey, map[string]interface{}{})
-	config.SetDefault(loggingConfigKey, map[string]interface{}{})
-	config.SetDefault(authConfigKey, map[string]interface{}{})
-	config.SetDefault(tokenConfigKey, map[string]interface{}{})
-	config.SetDefault(domainsConfigKey, []interface{}{})
 
 	return config
 }
@@ -82,42 +77,87 @@ func configDirectory() string {
 	return filepath.Dir(codeDir + "/../conf/")
 }
 
-// DBConfig returns the db connection config from the app config.
-// Panic in case of error. Only for tests and commands
-func DBConfig(appConfig *viper.Viper) *mysql.Config {
-	var dbConfig *mysql.Config
-	if err := appConfig.Sub(databaseConfigKey).Sub("connection").Unmarshal(&dbConfig); err != nil {
-		panic("Unable to load the 'database' configuration")
-	}
-	return dbConfig
+// ReplaceAuthConfig replaces the auth part of the config by the given one.
+func (app *Application) ReplaceAuthConfig(newGlobalConfig *viper.Viper) {
+	app.apiCtx.SetAuthConfig(AuthConfig(newGlobalConfig))
+	app.Config.Set(authConfigKey, newGlobalConfig.Get(authConfigKey))
 }
 
-// TokenConfig returns the token config from the app config
-// Panic in case of error. Only for tests and commands
-func TokenConfig(appConfig *viper.Viper) *token.Config {
-	config, err := token.Initialize(appConfig.Sub(tokenConfigKey))
+// ReplaceDomainsConfig replaces the domains part of the config by the given one.
+func (app *Application) ReplaceDomainsConfig(newGlobalConfig *viper.Viper) {
+	app.apiCtx.DomainConfig = DomainsConfig(newGlobalConfig)
+	app.Config.Set(domainsConfigKey, newGlobalConfig.Get(domainsConfigKey))
+}
+
+//
+// Subconfig accessors
+// These are public and working on the viper global config (as arg) to allow usage in app.New and every tests
+//
+
+// submapconfig returns the subconfig from a global config
+// only for configuration which are maps
+// Note: (kind of bug) for unmarshaled configs such as db, the config item needs to be set first (e.g., by
+// config file) for the env values to be read by viper
+
+func subconfig(globalConfig *viper.Viper, subconfigKey string) *viper.Viper {
+	globalConfig.SetDefault(subconfigKey, map[string]interface{}{})
+	subConfig := globalConfig.Sub(subconfigKey)
+	keyForEnv := strings.ReplaceAll(subconfigKey, ".", "__")
+	subConfig.SetEnvPrefix(fmt.Sprintf("%s_%s_", envPrefix, keyForEnv))
+	subConfig.AutomaticEnv()
+	return subConfig
+}
+
+// DBConfig returns the db connection fixed config from the global config.
+// Panic in case of unmarshaling error
+func DBConfig(globalConfig *viper.Viper) (config *mysql.Config) {
+	sub := subconfig(globalConfig, databaseConfigKey+".connection")
+	if err := sub.Unmarshal(&config); err != nil {
+		panic("Unable to load the 'database' configuration")
+	}
+	return
+}
+
+// TokenConfig returns the token fixed config from the global config
+// Panic in case of unmarshallign error
+func TokenConfig(globalConfig *viper.Viper) *token.Config {
+	sub := subconfig(globalConfig, tokenConfigKey)
+	config, err := token.Initialize(sub)
 	if err != nil {
 		panic("Unable to load the 'token' configuration")
 	}
 	return config
 }
 
-// AuthConfig returns the auth config from the app config
-// Only for tests and commands
-func AuthConfig(appConfig *viper.Viper) *viper.Viper {
-	return appConfig.Sub(authConfigKey)
+// AuthConfig returns an auth dynamic config from the global config
+// (env var changes impacts values)
+func AuthConfig(globalConfig *viper.Viper) *viper.Viper {
+	return subconfig(globalConfig, authConfigKey)
 }
 
-// LoggingConfig returns the logging config from the app config
-// Only for tests and commands
-func LoggingConfig(appConfig *viper.Viper) *viper.Viper {
-	return appConfig.Sub(loggingConfigKey)
+// LoggingConfig returns a logging dynamic config from the global config
+// (env var changes impacts values)
+func LoggingConfig(globalConfig *viper.Viper) *viper.Viper {
+	return subconfig(globalConfig, loggingConfigKey)
 }
 
-// DomainsConfig returns the domains config from the app config
-// Panic in case of error.
-func DomainsConfig(appConfig *viper.Viper) (config []domain.AppConfigItem) {
-	if err := appConfig.UnmarshalKey(domainsConfigKey, &config); err != nil {
+// ServerConfig returns a logging dynamic config from the global config
+// (env var changes impacts values)
+func ServerConfig(globalConfig *viper.Viper) *viper.Viper {
+	sub := subconfig(globalConfig, serverConfigKey)
+	sub.SetDefault("rootpath", "/")
+	sub.SetDefault("port", 8080)
+	sub.SetDefault("readTimeout", 60)
+	sub.SetDefault("writeTimeout", 60)
+	return sub
+}
+
+// DomainsConfig returns the domains fixed config from the global config
+// Panic in case of marshaling error.
+func DomainsConfig(globalConfig *viper.Viper) (config []domain.AppConfigItem) {
+	globalConfig.SetDefault(domainsConfigKey, []interface{}{})
+	// note that `.Sub` cannot be used to get a slice
+	if err := globalConfig.UnmarshalKey(domainsConfigKey, &config); err != nil {
 		panic("Unable to load 'Domains' config")
 	}
 	return
--- app/config_test.go
+++ app/config_test.go
@@ -7,12 +7,15 @@ import (
 	"testing"
 
 	"bou.ke/monkey"
+	"github.com/spf13/viper"
 	assertlib "github.com/stretchr/testify/assert"
 
 	"github.com/France-ioi/AlgoreaBackend/app/appenv"
+	"github.com/France-ioi/AlgoreaBackend/app/domain"
+	"github.com/France-ioi/AlgoreaBackend/app/token"
 )
 
-func TestLoad(t *testing.T) {
+func TestLoadConfigFrom(t *testing.T) {
 	assert := assertlib.New(t)
 	appenv.SetDefaultEnvToTest() // to ensure it tries to find the config.test file
 
@@ -31,12 +34,7 @@ func TestLoad(t *testing.T) {
 
 	// change default config values
 	fileName := filepath.Base(tmpFile.Name())
-	configName = fileName[:len(fileName)-5] // strip the ".yaml"
-	configDir = tmpDir
-	defer func() {
-		configName = defaultConfigName
-		configDir = configDirectory()
-	}()
+	configName := fileName[:len(fileName)-5] // strip the ".yaml"
 
 	tmpTestFileName := tmpDir + "/" + configName + ".test.yaml"
 	err = ioutil.WriteFile(tmpTestFileName, []byte("server:\n  rootpath: '/test/'"), 0644)
@@ -46,22 +44,23 @@ func TestLoad(t *testing.T) {
 	}()
 
 	_ = os.Setenv("ALGOREA_SERVER__WRITETIMEOUT", "999")
-	conf := Load()
+	conf := loadConfigFrom(configName, tmpDir)
 
 	// test config override
-	assert.EqualValues(1234, conf.Server.Port)
+	assert.EqualValues(1234, conf.Sub(serverConfigKey).GetInt("port"))
 
-	// test default
-	assert.EqualValues(60, conf.Server.ReadTimeout)
-
-	// test env
-	assert.EqualValues(999, conf.Server.WriteTimeout)
+	// test env variables
+	assert.EqualValues(999, conf.GetInt("server.WriteTimeout")) // does not work with Sub!
 
 	// test 'test' section
-	assert.EqualValues("/test/", conf.Server.RootPath)
+	assert.EqualValues("/test/", conf.Sub(serverConfigKey).GetString("RootPath"))
+
+	// test live env changes
+	_ = os.Setenv("ALGOREA_SERVER__WRITETIMEOUT", "777")
+	assert.EqualValues(777, conf.GetInt("server.WriteTimeout"))
 }
 
-func TestLoad_IgnoresMainConfigFileIfMissing(t *testing.T) {
+func TestLoadConfigFrom_IgnoresMainConfigFileIfMissing(t *testing.T) {
 	assert := assertlib.New(t)
 	appenv.SetDefaultEnvToTest() // to ensure it tries to find the config.test file
 
@@ -70,74 +69,148 @@ func TestLoad_IgnoresMainConfigFileIfMissing(t *testing.T) {
 	defer deferFunc()
 
 	fileName := filepath.Base(tmpFile.Name())
-	configName = fileName[:len(fileName)-10] // strip the ".test.yaml"
-	configDir = os.TempDir()
-	defer func() {
-		configName = defaultConfigName
-		configDir = configDirectory()
-	}()
+	configName := fileName[:len(fileName)-10] // strip the ".test.yaml"
 
-	conf := Load()
+	conf := loadConfigFrom(configName, os.TempDir())
 	assert.NotNil(conf)
 }
 
-func TestLoad_IgnoresEnvConfigFileIfMissing(t *testing.T) {
+func TestLoadConfigFrom_IgnoresEnvConfigFileIfMissing(t *testing.T) {
 	assert := assertlib.New(t)
 	appenv.SetDefaultEnvToTest() // to ensure it tries to find the config.test file
 
 	// create a temp config file
-	tmpDir := os.TempDir()
 	tmpFile, deferFunc := createTmpFile("config-*.yaml", assert)
 	defer deferFunc()
 
 	fileName := filepath.Base(tmpFile.Name())
-	configName = fileName[:len(fileName)-5] // strip the ".yaml"
-	configDir = tmpDir
-	defer func() {
-		configName = defaultConfigName
-		configDir = configDirectory()
-	}()
+	configName := fileName[:len(fileName)-5] // strip the ".yaml"
 
-	conf := Load()
+	conf := loadConfigFrom(configName, os.TempDir())
+
 	assert.NotNil(conf)
 }
 
-func TestLoad_CannotUnmarshal(t *testing.T) {
-	assert := assertlib.New(t)
-
-	// create a temp config file
-	tmpFile, deferFunc := createTmpFile("config-*.yaml", assert)
-	defer deferFunc()
-
-	text := []byte("unknown: 1234\n")
-	_, err := tmpFile.Write(text)
-	assert.NoError(err)
-
-	// change default config values
-	fileName := filepath.Base(tmpFile.Name())
-	configName = fileName[:len(fileName)-5] // strip the ".yaml"
-	configDir = os.TempDir()
-	defer func() {
-		configName = defaultConfigName
-		configDir = configDirectory()
-	}()
-
-	called := false
-	monkey.Patch(os.Exit, func(int) { called = true })
-	defer monkey.UnpatchAll()
-	Load()
-	assert.True(called)
-}
-
-func TestLoad_Concurrent(t *testing.T) {
+func TestLoadConfig_Concurrent(t *testing.T) {
 	_ = os.Unsetenv("ALGOREA_ENV")
 	appenv.SetDefaultEnvToTest()
 	assert := assertlib.New(t)
 	assert.NotPanics(func() {
-		Load()
+		LoadConfig()
 		for i := 0; i < 1000; i++ {
-			go func() { Load() }()
+			go func() { LoadConfig() }()
 		}
+	})
+}
+
+func TestDBConfig_Success(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	_ = os.Setenv("", "myself")
+	globalConfig.Set("database.connection.collation", "stuff")
+	globalConfig.Set("database.connection.TLSConfig", "v88")
+	// Still buggy, for unmarshaled config, the config needs to be set first (by config file
+	// or manually) to allow setting it through env
+	_ = os.Setenv("ALGOREA_DATABASE__CONNECTION__TLSCONFIG", "v99")
+	defer func() { _ = os.Unsetenv("ALGOREA_DATABASE__CONNECTION__TLSCONFIG") }()
+	dbConfig := DBConfig(globalConfig)
+	assert.Equal("stuff", dbConfig.Collation)
+	assert.Equal("v99", dbConfig.TLSConfig)
+}
+
+func TestDBConfig_Panic(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("database.connection.Timeout", "invalid")
+	assert.Panics(func() {
+		_ = DBConfig(globalConfig)
+	})
+}
+
+func TestTokenConfig_Success(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	monkey.Patch(token.Initialize, func(config *viper.Viper) (*token.Config, error) {
+		return &token.Config{PlatformName: "test"}, nil
+	})
+	defer monkey.UnpatchAll()
+	config := TokenConfig(globalConfig)
+	assert.Equal("test", config.PlatformName)
+}
+
+func TestTokenConfig_Panic(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("token.PublicKeyFile", "notafile")
+	assert.Panics(func() {
+		_ = TokenConfig(globalConfig)
+	})
+}
+
+func TestAuthConfig(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("auth.anykey", 42)
+	config := AuthConfig(globalConfig)
+	assert.Equal(42, config.GetInt("anykey"))
+	_ = os.Setenv("ALGOREA_AUTH__ANYKEY", "999")
+	assert.Equal(999, config.GetInt("anykey"))
+}
+
+func TestLoggingConfig(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("logging.anykey", 42)
+	config := LoggingConfig(globalConfig)
+	assert.Equal(42, config.GetInt("anykey"))
+	_ = os.Setenv("ALGOREA_LOGGING__ANYKEY", "999")
+	assert.Equal(999, config.GetInt("anykey"))
+}
+
+func TestServerConfig(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("server.anykey", 42)
+	config := ServerConfig(globalConfig)
+	assert.Equal(42, config.GetInt("anykey"))
+	_ = os.Setenv("ALGOREA_SERVER__ANYKEY", "999")
+	assert.Equal(999, config.GetInt("anykey"))
+	// test default values:
+	assert.Equal("/", config.GetString("rootpath"))
+	assert.Equal(8080, config.GetInt("port"))
+	assert.Equal(60, config.GetInt("readTimeout"))
+	assert.Equal(60, config.GetInt("writeTimeout"))
+}
+
+func TestDomainsConfig_Success(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	sampleDomain := domain.AppConfigItem{
+		Domains:       []string{"localhost", "other"},
+		RootGroup:     1,
+		RootSelfGroup: 2,
+		RootTempGroup: 3,
+	}
+	globalConfig.Set("domains", []domain.AppConfigItem{sampleDomain})
+	config := DomainsConfig(globalConfig)
+	assert.Len(config, 1)
+	assert.Equal(sampleDomain, config[0])
+}
+
+func TestDDomainsConfig_Empty(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("domains", []string{})
+	config := DomainsConfig(globalConfig)
+	assert.Len(config, 0)
+}
+
+func TestDomainsConfig_Panic(t *testing.T) {
+	assert := assertlib.New(t)
+	globalConfig := viper.New()
+	globalConfig.Set("domains", []int{1, 2})
+	assert.Panics(func() {
+		_ = DomainsConfig(globalConfig)
 	})
 }
 
