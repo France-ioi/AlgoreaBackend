1bf021024bc4be15d8edd5b519003c6933202faa
Dmitry Zenovich <dzenovich@gmail.com>
2020-04-24 15:36:58 +0300

    do not load test environment before loading the real environment

--- testhelpers/db.go
+++ testhelpers/db.go
@@ -19,13 +19,6 @@ import (
 
 const fixtureDir = "testdata" // special directory which is not included in binaries by the compile
 
-func init() { // nolint:gochecknoinits
-	appenv.SetDefaultEnvToTest()
-	conf := config.Load()
-	// Apply the config to the global logger
-	logging.SharedLogger.Configure(conf.Logging)
-}
-
 // SetupDBWithFixture creates a new DB connection, empties the DB, and loads a fixture
 func SetupDBWithFixture(fixtureNames ...string) *database.DB {
 	mustNotBeInProdEnv()
---
+++ testhelpers/db_test.go
@@ -0,0 +1,14 @@
+package testhelpers_test
+
+import (
+	"github.com/France-ioi/AlgoreaBackend/app/appenv"
+	"github.com/France-ioi/AlgoreaBackend/app/config"
+	"github.com/France-ioi/AlgoreaBackend/app/logging"
+)
+
+func init() { // nolint:gochecknoinits
+	appenv.SetDefaultEnvToTest()
+	conf := config.Load()
+	// Apply the config to the global logger
+	logging.SharedLogger.Configure(conf.Logging)
+}
