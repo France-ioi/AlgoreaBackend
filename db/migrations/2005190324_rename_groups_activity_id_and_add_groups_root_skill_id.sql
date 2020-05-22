42675e63e344166ea25faea556e09f4cf5825243
Dmitry Zenovich <dzenovich@gmail.com>
2020-05-22 17:32:26 +0300

    Revert "optimize /db/migrations/2005190324_rename_groups_activity_id_and_add_groups_root_skill_id.sql"
    This reverts commit 44793d81d79f2cfc512c6a378933d21ce30d1cfa.

--- db/migrations/2005190324_rename_groups_activity_id_and_add_groups_root_skill_id.sql
+++ db/migrations/2005190324_rename_groups_activity_id_and_add_groups_root_skill_id.sql
@@ -1,24 +1,22 @@
 -- +migrate Up
 ALTER TABLE `groups`
-    DROP FOREIGN KEY `fk_groups_activity_id_items_id`,
-    DROP KEY `fk_groups_activity_id_items_id`;
-ALTER TABLE `groups`
     RENAME COLUMN `activity_id` TO `root_activity_id`,
-    ADD COLUMN `root_skill_id` BIGINT(20) DEFAULT NULL COMMENT 'Root skill associated with this group'
-        AFTER `root_activity_id`,
+    DROP FOREIGN KEY `fk_groups_activity_id_items_id`,
+    DROP KEY `fk_groups_activity_id_items_id`,
     ADD CONSTRAINT `fk_groups_root_activity_id_items_id`
         FOREIGN KEY (`root_activity_id`) REFERENCES `items`(`id`) ON DELETE SET NULL,
+    ADD COLUMN `root_skill_id` BIGINT(20) DEFAULT NULL COMMENT 'Root skill associated with this group'
+        AFTER `root_activity_id`,
     ADD CONSTRAINT `fk_groups_root_skill_id_items_id`
         FOREIGN KEY (`root_skill_id`) REFERENCES `items`(`id`) ON DELETE SET NULL;
 
 -- +migrate Down
 ALTER TABLE `groups`
-    DROP FOREIGN KEY `fk_groups_root_skill_id_items_id`,
-    DROP FOREIGN KEY `fk_groups_root_activity_id_items_id`,
-    DROP KEY `fk_groups_root_skill_id_items_id`,
-    DROP KEY `fk_groups_root_activity_id_items_id`,
-    DROP COLUMN `root_skill_id`;
-ALTER TABLE `groups`
     RENAME COLUMN `root_activity_id` TO `activity_id`,
+    DROP FOREIGN KEY `fk_groups_root_activity_id_items_id`,
+    DROP KEY `fk_groups_root_activity_id_items_id`,
     ADD CONSTRAINT `fk_groups_activity_id_items_id`
-        FOREIGN KEY (`activity_id`) REFERENCES `items`(`id`) ON DELETE SET NULL;
+        FOREIGN KEY (`activity_id`) REFERENCES `items`(`id`) ON DELETE SET NULL,
+    DROP FOREIGN KEY `fk_groups_root_skill_id_items_id`,
+    DROP KEY `fk_groups_root_skill_id_items_id`,
+    DROP COLUMN `root_skill_id`;
