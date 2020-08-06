dd2f0199be3f2c225df779716382bd544c60b71d
Dmitry Zenovich <dzenovich@gmail.com>
2020-08-07 02:44:18 +0300

    introduce a new cli command db-recompute

---
+++ cmd/db_recompute.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql" // use to force database/sql to use mysql
+	"github.com/spf13/cobra"
+
+	"github.com/France-ioi/AlgoreaBackend/app"
+	"github.com/France-ioi/AlgoreaBackend/app/appenv"
+)
+
+func init() { // nolint:gochecknoinits
+
+	var recomputeCmd = &cobra.Command{
+		Use:   "db-recompute [environment]",
+		Short: "recompute db caches",
+		Long:  `recompute runs recalculation of db caches (groups ancestors, items ancestors, cached permissions, attempt results)`,
+		Args:  cobra.MaximumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			var err error
+
+			// if arg given, replace the env
+			if len(args) > 0 {
+				appenv.SetEnv(args[0])
+			}
+
+			appenv.SetDefaultEnvToTest()
+
+			// open DB
+			application, err := app.New()
+			if err != nil {
+				fmt.Println("Fatal error: ", err)
+				os.Exit(1)
+			}
+
+			assertNoError(recomputeDBCaches(application.Database), "Cannot recompute db caches")
+
+			// Success
+			fmt.Println("DONE")
+		},
+	}
+
+	rootCmd.AddCommand(recomputeCmd)
+}
