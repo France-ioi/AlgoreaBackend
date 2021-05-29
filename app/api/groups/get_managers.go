6e43b22fd3f2f768c7f1f85cfa8121447428ee1e
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-05-30 02:39:16 +0300

    display logins of users in groupManagersView

--- app/api/groups/get_managers.go
+++ app/api/groups/get_managers.go
@@ -10,6 +10,8 @@ import (
 
 // GroupManagersViewResponseRowUser contains names of a manager
 type GroupManagersViewResponseRowUser struct {
+	// Displayed only for users
+	Login string `json:"login"`
 	// Nullable; displayed only for users
 	FirstName *string `json:"first_name"`
 	// Nullable; displayed only for users
@@ -147,7 +149,7 @@ func (srv *Service) getManagers(w http.ResponseWriter, r *http.Request) service.
 		query = query.
 			Joins("JOIN groups_ancestors_active ON groups_ancestors_active.ancestor_group_id = group_managers.group_id").
 			Where("groups_ancestors_active.child_group_id = ?", groupID).
-			Select(`groups.id, groups.name, groups.type, users.first_name, users.last_name,
+			Select(`groups.id, groups.name, groups.type, users.first_name, users.last_name, users.login,
 			        MAX(IF(groups_ancestors_active.is_self, can_manage_value, 1)) AS can_manage_value,
 			        MAX(IF(groups_ancestors_active.is_self, can_grant_group_access, 0)) AS can_grant_group_access,
 			        MAX(IF(groups_ancestors_active.is_self, can_watch_members, 0)) AS can_watch_members,
@@ -157,7 +159,7 @@ func (srv *Service) getManagers(w http.ResponseWriter, r *http.Request) service.
 			Group("groups.id")
 	} else {
 		query = query.Where("group_managers.group_id = ?", groupID).
-			Select(`groups.id, groups.name, groups.type, users.first_name, users.last_name,
+			Select(`groups.id, groups.name, groups.type, users.first_name, users.last_name, users.login,
               can_manage_value, can_grant_group_access, can_watch_members`)
 	}
 
