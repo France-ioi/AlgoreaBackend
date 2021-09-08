d2dd67323ab88033bb7d665396136b5bc2674d34
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-09-08 12:27:14 +0300

    preprocess value in TheTemplateConstantIsString() and TheTemplateConstantIsDocString()

--- testhelpers/steps_template.go
+++ testhelpers/steps_template.go
@@ -7,11 +7,21 @@ import (
 )
 
 func (ctx *TestContext) TheTemplateConstantIsString(name, value string) error { // nolint
+	value, err := ctx.preprocessString(value)
+	if err != nil {
+		return err
+	}
+
 	ctx.templateSet.AddGlobal(name, value)
 	return nil
 }
 
 func (ctx *TestContext) TheTemplateConstantIsDocString(name string, value *messages.PickleStepArgument_PickleDocString) error { // nolint
-	ctx.templateSet.AddGlobal(name, value.Content)
+	preprocessedValue, err := ctx.preprocessString(value.Content)
+	if err != nil {
+		return err
+	}
+
+	ctx.templateSet.AddGlobal(name, preprocessedValue)
 	return nil
 }
