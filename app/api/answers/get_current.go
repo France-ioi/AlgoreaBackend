10006c2505b28f7f648efddb7563ac8021b8005e
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-11-29 06:06:01 +0300

    move withGradings() from app/api/answers/get_current.go into app/api/answers/answers.go

--- app/api/answers/get_current.go
+++ app/api/answers/get_current.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/go-chi/render"
 
-	"github.com/France-ioi/AlgoreaBackend/app/database"
 	"github.com/France-ioi/AlgoreaBackend/app/service"
 )
 
@@ -79,12 +78,4 @@ func (srv *Service) getCurrentAnswer(rw http.ResponseWriter, httpReq *http.Reque
 
 	render.Respond(rw, httpReq, convertedResult)
 	return service.NoError
-}
-
-func withGradings(answersQuery *database.DB) *database.DB {
-	return answersQuery.
-		Joins("LEFT JOIN gradings ON gradings.answer_id = answers.id").
-		Select(`answers.id, answers.author_id, answers.item_id, answers.attempt_id, answers.participant_id,
-			answers.type, answers.state, answers.answer, answers.created_at, gradings.score,
-			gradings.graded_at`)
 }
