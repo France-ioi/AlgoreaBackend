de79fb30fefc1181356f02696a3cd1da5b730a9e
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-12-04 19:19:41 +0300

    fix the comment of results.help_requested

--- db/migrations/2112041553_add_column_results_help_requested.sql
+++ db/migrations/2112041553_add_column_results_help_requested.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 ALTER TABLE `results`
   ADD COLUMN `help_requested` TINYINT(1) NOT NULL DEFAULT 0
-    COMMENT 'Whether the participant has requested help for the item in this attempt'
+    COMMENT 'Whether the participant is requesting help on the item in this attempt'
     AFTER `latest_hint_at`;
 
 -- +migrate Down
