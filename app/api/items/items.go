5e21eb97a701b97beea8c96e4d966fdc3ecc9bb3
Geoffrey Huck <geoffrey@geot.fr>
2023-02-08 10:56:37 +0100

    Service list all possible breadcrumbs for a started item from a text_id

--- app/api/items/items.go
+++ app/api/items/items.go
@@ -4,7 +4,6 @@ package items
 import (
 	"errors"
 	"fmt"
-
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 
@@ -68,7 +67,8 @@ func (srv *Service) SetRoutes(router chi.Router) {
 	routerWithParticipant.Post("/items/{ids:(\\d+/)+}start-result", service.AppHandler(srv.startResult).ServeHTTP)
 	routerWithParticipant.Post("/items/{ids:(\\d+/)+}start-result-path", service.AppHandler(srv.startResultPath).ServeHTTP)
 	routerWithParticipant.Get("/items/{item_id}/path-from-root", service.AppHandler(srv.getPathFromRoot).ServeHTTP)
-	router.Get("/items/{item_id}/breadcrumbs-from-roots", service.AppHandler(srv.getBreadcrumbsFromRoots).ServeHTTP)
+	router.Get("/items/{item_id}/breadcrumbs-from-roots", service.AppHandler(srv.getBreadcrumbsFromRootsByItemID).ServeHTTP)
+	router.Get("/items/by-text-id/{text_id}/breadcrumbs-from-roots", service.AppHandler(srv.getBreadcrumbsFromRootsByTextId).ServeHTTP)
 }
 
 func checkHintOrScoreTokenRequiredFields(user *database.User, taskToken *token.Task, otherTokenFieldName string,
