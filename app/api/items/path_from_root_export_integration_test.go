66bf48264fe80352d01bf803c6b5b0dcfcb97c8f
Geoffrey Huck <geoffrey@geot.fr>
2023-07-19 13:03:15 +0200

    findItemPath function is now public, and returns the info about whether the path is active This function was currently only used for the itemPathFromRootFind service We want to use this function for the itemBreadcrumbsFromRootsGet service also, because we want non-active path to be returned as well by this service, and we want to know whether a returned path is active or not The only difference is that the itemPathFromRootFind service needs only the first path found, while the itemBreadcrumbsFromRootsGet needs them all

--- app/api/items/path_from_root_export_integration_test.go
+++
@@ -1,9 +0,0 @@
-//go:build !unit
-
-package items
-
-import "github.com/France-ioi/AlgoreaBackend/app/database"
-
-func FindItemPath(store *database.DataStore, participantID, itemID int64) []string {
-	return findItemPath(store, participantID, itemID)
-}
