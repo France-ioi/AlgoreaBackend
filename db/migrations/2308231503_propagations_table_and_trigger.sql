ea6a57cb6dfe64d1ac95d658b8fff29b8b32bbee
Geoffrey Huck <geoffrey@geot.fr>
2023-08-29 12:52:00 +0200

    Make sure trigger for propagation is never called again before it is completed We use a counter in the database that is increment when the trigger is called, so we can easily test whether the trigger was executed or not We use an int64 so we guarantee we will never reach int limit

--- db/migrations/2308231503_propagations_table_and_trigger.sql
+++ db/migrations/2308231503_propagations_table_and_trigger.sql
@@ -3,15 +3,23 @@
 CREATE TABLE `propagations` (
   `propagation_id` BIGINT NOT NULL,
   `propagate` TINYINT DEFAULT 0 COMMENT 'MUST be set to 0. Is set in 1 in a trigger.',
+  `scheduled_counter` BIGINT DEFAULT 0 COMMENT 'Set in Trigger only, used to make sure propagation is not triggered more than once before it is executed.',
   PRIMARY KEY (`propagation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Used to know if a propagation has to be run. Has trigger to call propagation via AWS Lambda.';
 
 -- +migrate StatementBegin
 CREATE TRIGGER `before_update_propagation` BEFORE UPDATE ON `propagations` FOR EACH ROW
 BEGIN
-  # This must be set on AWS Aurora.
-  -- lambda_async('arn:aws:lambda:REGION:ACCOUNT_ID:function:Propagate', '{}');
-  SET NEW.propagate = 1;
+  IF OLD.propagate = 0 THEN
+    # This must be set on AWS Aurora.
+    -- lambda_async('arn:aws:lambda:REGION:ACCOUNT_ID:function:Propagate', '{}');
+
+    -- Is incremented only when a propagation is scheduled and no propagation is scheduled yet.
+    -- Used to test that we don't call the Lambda Trigger when not needed.
+    SET NEW.scheduled_counter = OLD.scheduled_counter + 1;
+
+    SET NEW.propagate = 1;
+  END IF;
 END;
 -- +migrate StatementEnd
 
