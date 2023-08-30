ecc442aeb5a327603616ec80c753999fff3ca980
Geoffrey Huck <geoffrey@geot.fr>
2023-08-30 16:23:45 +0200

    FindItemPath: Add test to make sure that is_started refers to the participant and to the user It was already the case, but now we're certain it is checked

--- app/api/items/path_from_root_integration_test.go
+++ app/api/items/path_from_root_integration_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/France-ioi/AlgoreaBackend/testhelpers"
 )
 
-func Test_FindItemPathFirstPathOnly(t *testing.T) {
+func Test_FindItemPath(t *testing.T) {
 	type args struct {
 		participantID int64
 		itemID        int64
@@ -742,6 +742,51 @@ func Test_FindItemPathFirstPathOnly(t *testing.T) {
 				{Path: []string{"100", "101"}, IsStarted: true},
 			},
 		},
+		{
+			name: "is_started should be true when the path is started by the participant, not if only by the user",
+			fixture: `
+					groups:
+						- {id: 998, root_activity_id: 1000}
+						- {id: 999, root_activity_id: 1001}
+						- {id: 1000}
+						- {id: 1001}
+					groups_groups:
+						- {parent_group_id: 998, child_group_id: 1000}
+						- {parent_group_id: 998, child_group_id: 1001}
+						- {parent_group_id: 999, child_group_id: 1000}
+						- {parent_group_id: 999, child_group_id: 1001}
+					items:
+						- {id: 1000, default_language_tag: fr}
+						- {id: 1001, default_language_tag: fr}
+					items_items:
+						- {parent_item_id: 1000, child_item_id: 1001, child_order: 1}
+					permissions_generated:
+						- {group_id: 1000, item_id: 1000, can_view_generated: content}
+						- {group_id: 1000, item_id: 1001, can_view_generated: content}
+						- {group_id: 1001, item_id: 1000, can_view_generated: content}
+						- {group_id: 1001, item_id: 1001, can_view_generated: content}
+					attempts:
+						- {participant_id: 1000, id: 0}
+						- {participant_id: 1001, id: 0}
+					results:
+						- {participant_id: 1000, attempt_id: 0, item_id: 1001, started_at: 2020-01-01 01:01:01}
+						- {participant_id: 1001, attempt_id: 0, item_id: 1000, started_at: 2020-01-01 01:01:01}
+						- {participant_id: 1001, attempt_id: 0, item_id: 1001, started_at: 2020-01-01 01:01:01}
+				`,
+			args: args{
+				participantID: 1000,
+				itemID:        1001,
+				user: &database.User{
+					GroupID: 1001,
+				},
+				pathRootBy: items.PathRootUser,
+				limit:      0,
+			},
+			want: []items.ItemPath{
+				{Path: []string{"1001"}, IsStarted: true},
+				{Path: []string{"1000", "1001"}, IsStarted: false},
+			},
+		},
 	}
 	const globalFixture = `
 		groups: [{id: 100}, {id: 101}, {id: 200, root_activity_id: 1, root_skill_id: 3}]
@@ -777,7 +822,7 @@ func Test_FindItemPathFirstPathOnly(t *testing.T) {
 			}))
 			got = items.FindItemPaths(
 				store,
-				&database.User{},
+				tt.args.user,
 				tt.args.participantID,
 				tt.args.itemID,
 				tt.args.pathRootBy,
