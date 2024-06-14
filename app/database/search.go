8fe53b2be2601f82863694599c838e6c8199ef28
Geoffrey Huck <geoffrey@geot.fr>
2024-06-14 17:51:54 +0200

    Use fulltext indexes for search.
    Signed-off-by: Geoffrey Huck <geoffrey@geot.fr>

--- app/database/search.go
+++ app/database/search.go
@@ -1,19 +1,11 @@
 package database
 
-import "strings"
-
 // WhereSearchStringMatches returns a composable query where {field} matches the search string {searchString}
 // All the words in the search string are matched with "AND".
 func (conn *DB) WhereSearchStringMatches(field, searchString string) *DB {
-	escapedSearchString := EscapeLikeString(searchString, '|')
-
 	query := conn.db
 
-	// For each word in the escaped search string.
-	for _, word := range strings.Fields(escapedSearchString) {
-		// Add a condition to the query to match the word.
-		query = query.Where(field+" LIKE CONCAT('%', ?, '%') ESCAPE '|'", word)
-	}
+	query = query.Where("MATCH ("+field+") AGAINST (? IN BOOLEAN MODE)", searchString)
 
 	return newDB(conn.ctx, query)
 }
