d1a07d49f6ff6ebc4a8fd5fcf73a04c2f08270a0
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2024-08-30 19:01:48 +0300

    introduce --delay parameter of "delete-temp-users" command allowing to delay deletions of temporary users with expired sessions

--- cmd/delete_temp_users.go
+++ cmd/delete_temp_users.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql" // use to force database/sql to use mysql
 	"github.com/spf13/cobra"
@@ -14,10 +15,23 @@ import (
 
 // nolint:gosec
 func init() { //nolint:gochecknoinits,gocyclo
+	var delay time.Duration
+
 	deleteTempUsersCmd := &cobra.Command{
 		Use:   "delete-temp-users [environment]",
 		Short: "delete all temporary users with expired sessions",
+		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			if delay < 0 {
+				fmt.Println("delay must be positive or equal to 0")
+				os.Exit(1)
+			}
+
+			if delay > 100*365*24*time.Hour {
+				fmt.Println("delay must be less than 100 years")
+				os.Exit(1)
+			}
+
 			// if arg given, replace the env
 			if len(args) > 0 {
 				appenv.SetEnv(args[0])
@@ -33,7 +47,7 @@ func init() { //nolint:gochecknoinits,gocyclo
 				os.Exit(1)
 			}
 
-			err = database.NewDataStore(application.Database).Users().DeleteTemporaryWithTraps()
+			err = database.NewDataStore(application.Database).Users().DeleteTemporaryWithTraps(delay)
 			if err != nil {
 				fmt.Println("Fatal error: ", err)
 				os.Exit(1)
@@ -43,6 +57,9 @@ func init() { //nolint:gochecknoinits,gocyclo
 			fmt.Println("DONE")
 		},
 	}
+
+	deleteTempUsersCmd.Flags().DurationVar(&delay, "delay", 60*24*time.Hour,
+		"delay between expiration and deletion of temporary users")
 
 	rootCmd.AddCommand(deleteTempUsersCmd)
 }
