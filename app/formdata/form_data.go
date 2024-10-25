f6c69c70fbfa713a4f7c0be5700b4f0b4224b911
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2024-10-25 13:08:26 +0300

    add a comment on FormData.ParseJSONRequestData() warning that the method can be called only once for an http.Request

--- app/formdata/form_data.go
+++ app/formdata/form_data.go
@@ -127,6 +127,7 @@ func (f *FormData) AllowUnknownFields() {
 }
 
 // ParseJSONRequestData parses and validates JSON from the request according to the structure definition.
+// As it reads out the request body, it should be called only once.
 func (f *FormData) ParseJSONRequestData(r *http.Request) error {
 	if err := f.decodeRequestJSONDataIntoStruct(r); err != nil {
 		return err
