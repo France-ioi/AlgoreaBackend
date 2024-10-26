b1faaa678cc31478841c2a7e5ba4eb80d7bc569a
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2024-10-26 05:14:28 +0300

    suppress output of passing integration tests in app/service

--- app/service/propagation_integration_test.go
+++ app/service/propagation_integration_test.go
@@ -64,6 +64,8 @@ func TestSchedulePropagation(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			testhelpers.SuppressOutputIfPasses(t)
+
 			db := testhelpers.SetupDBWithFixtureString(`
 				groups:
 					- {id: 1, type: Class}
