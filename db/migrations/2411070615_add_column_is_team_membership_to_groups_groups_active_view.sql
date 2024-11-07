373af709871421083c138127834e21db2091b9ba
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2024-11-07 10:06:30 +0300

    add is_team_membership column to groups_groups_active view

---
+++ db/migrations/2411070615_add_column_is_team_membership_to_groups_groups_active_view.sql
@@ -0,0 +1,17 @@
+-- +migrate Up
+DROP VIEW groups_groups_active;
+CREATE VIEW groups_groups_active AS SELECT * FROM groups_groups WHERE NOW() < expires_at;
+
+-- +migrate Down
+DROP VIEW groups_groups_active;
+CREATE VIEW `groups_groups_active` AS
+  SELECT `groups_groups`.`parent_group_id` AS `parent_group_id`,
+         `groups_groups`.`child_group_id` AS `child_group_id`,
+         `groups_groups`.`expires_at` AS `expires_at`,
+         `groups_groups`.`personal_info_view_approved_at` AS `personal_info_view_approved_at`,
+         `groups_groups`.`personal_info_view_approved` AS `personal_info_view_approved`,
+         `groups_groups`.`lock_membership_approved_at` AS `lock_membership_approved_at`,
+         `groups_groups`.`lock_membership_approved` AS `lock_membership_approved`,
+         `groups_groups`.`watch_approved_at` AS `watch_approved_at`,
+         `groups_groups`.`watch_approved` AS `watch_approved`
+  FROM `groups_groups` WHERE (NOW() < `groups_groups`.`expires_at`);
