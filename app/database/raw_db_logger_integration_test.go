371ce4b72fe9afef5feb46c3e79abf60b890f310
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2024-12-07 02:54:56 +0300

    introduce a new wrapper for MySQL driver which resets the connection (sends COM_RESET_CONNECTION) before reusing, employ this new driver wrapper everywhere

--- app/database/raw_db_logger_integration_test.go
+++ app/database/raw_db_logger_integration_test.go
@@ -59,3 +59,48 @@ func Test_RawSQLQueryLogging_Duration(t *testing.T) {
 		}
 	}
 }
+
+func Test_RawSQLQueryLogging_ResetSession(t *testing.T) {
+	testoutput.SuppressIfPasses(t)
+
+	monkey.PatchInstanceMethod(reflect.TypeOf(&logging.Logger{}), "IsRawSQLQueriesLoggingEnabled",
+		func(_ *logging.Logger) bool { return true })
+	defer monkey.UnpatchAll()
+
+	sqlDB, err := testhelpers.OpenRawDBConnection()
+	require.NoError(t, err)
+	defer func() { _ = sqlDB.Close() }()
+
+	loggerHook, loggerRestoreFunc := logging.MockSharedLoggerHook()
+	defer loggerRestoreFunc()
+
+	db, err := database.OpenWithLogConfig(sqlDB, database.LogConfig{LogSQLQueries: true}, true)
+	require.NoError(t, err)
+	defer func() { _ = db.Close() }()
+
+	var result []interface{}
+	require.NoError(t, db.Raw("SELECT 1").Scan(&result).Error())
+	require.NoError(t, db.Raw("SELECT 1").Scan(&result).Error())
+
+	entries := loggerHook.AllEntries()
+	expectedMessages := []string{
+		"sql-conn-reset", "SELECT 1", "sql-conn-reset", "SELECT 1",
+	}
+	require.Len(t, entries, len(expectedMessages))
+
+	for i, entry := range entries {
+		assert.Equal(t, expectedMessages[i], entry.Message)
+		assert.Equal(t, "db", entry.Data["type"])
+		assert.Contains(t, entry.Data, "duration")
+		if duration, ok := entry.Data["duration"]; ok {
+			assert.IsType(t, "", duration)
+			if durationStr, ok := duration.(string); ok {
+				parsedDuration, err := time.ParseDuration(durationStr)
+				assert.NoError(t, err)
+				if err == nil {
+					assert.Greater(t, parsedDuration, 0*time.Second)
+				}
+			}
+		}
+	}
+}
