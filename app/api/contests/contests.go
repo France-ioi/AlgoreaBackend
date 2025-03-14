0ad4f12f95ece8c021787d76f2a36ef0f0b02317
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2025-03-14 05:26:08 +0300

    move itemAdditionalTimesInfo & getParticipantTypeForItemWithDurationManagedByUser() into api/contests/get_members_additional_times.go

--- app/api/contests/contests.go
+++ app/api/contests/contests.go
@@ -6,7 +6,6 @@ import (
 	"github.com/go-chi/render"
 
 	"github.com/France-ioi/AlgoreaBackend/v2/app/auth"
-	"github.com/France-ioi/AlgoreaBackend/v2/app/database"
 	"github.com/France-ioi/AlgoreaBackend/v2/app/service"
 )
 
@@ -27,29 +26,4 @@ func (srv *Service) SetRoutes(router chi.Router) {
 		service.AppHandler(srv.getGroupAdditionalTimes).ServeHTTP)
 	router.Get("/items/{item_id}/groups/{group_id}/members/additional-times",
 		service.AppHandler(srv.getMembersAdditionalTimes).ServeHTTP)
-}
-
-// swagger:model itemAdditionalTimesInfo
-type itemAdditionalTimesInfo struct {
-	// required: true
-	GroupID int64 `json:"group_id,string"`
-	// required: true
-	Name string `json:"name"`
-	// required: true
-	Type string `json:"type"`
-	// required: true
-	AdditionalTime int32 `json:"additional_time"`
-	// required: true
-	TotalAdditionalTime int32 `json:"total_additional_time"`
-}
-
-const team = "Team"
-
-func getParticipantTypeForItemWithDurationManagedByUser(
-	store *database.DataStore, itemID int64, user *database.User,
-) (string, error) {
-	var participantType string
-	err := store.Items().WithDurationByIDAndManagedByUser(itemID, user).
-		PluckFirst("items.entry_participant_type", &participantType).Error()
-	return participantType, err
 }
--- app/api/contests/get_members_additional_times.go
+++ app/api/contests/get_members_additional_times.go
@@ -6,8 +6,23 @@ import (
 	"github.com/go-chi/render"
 	"github.com/jinzhu/gorm"
 
+	"github.com/France-ioi/AlgoreaBackend/v2/app/database"
 	"github.com/France-ioi/AlgoreaBackend/v2/app/service"
 )
+
+// swagger:model itemAdditionalTimesInfo
+type itemAdditionalTimesInfo struct {
+	// required: true
+	GroupID int64 `json:"group_id,string"`
+	// required: true
+	Name string `json:"name"`
+	// required: true
+	Type string `json:"type"`
+	// required: true
+	AdditionalTime int32 `json:"additional_time"`
+	// required: true
+	TotalAdditionalTime int32 `json:"total_additional_time"`
+}
 
 // swagger:operation GET /items/{item_id}/groups/{group_id}/members/additional-times items itemListMembersAdditionalTime
 //
@@ -161,4 +176,13 @@ func (srv *Service) getMembersAdditionalTimes(w http.ResponseWriter, r *http.Req
 
 	render.Respond(w, r, result)
 	return service.NoError
+}
+
+func getParticipantTypeForItemWithDurationManagedByUser(
+	store *database.DataStore, itemID int64, user *database.User,
+) (string, error) {
+	var participantType string
+	err := store.Items().WithDurationByIDAndManagedByUser(itemID, user).
+		PluckFirst("items.entry_participant_type", &participantType).Error()
+	return participantType, err
 }
