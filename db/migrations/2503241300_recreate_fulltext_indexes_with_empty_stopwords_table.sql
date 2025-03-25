1d6e952b6ab788889133dc5f5f7ee86c73b7d80c
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2025-03-25 12:56:34 +0300

    restore innodb_ft_server_stopword_table after applying/undoing db/migrations/2503241300_recreate_fulltext_indexes_with_empty_stopwords_table.sql

--- db/migrations/2503241300_recreate_fulltext_indexes_with_empty_stopwords_table.sql
+++ db/migrations/2503241300_recreate_fulltext_indexes_with_empty_stopwords_table.sql
@@ -1,4 +1,5 @@
 -- +migrate Up
+SET @old_stopword_table := @@innodb_ft_server_stopword_table;
 SET GLOBAL innodb_ft_server_stopword_table = CONCAT(DATABASE(), '/stopwords');
 
 ALTER TABLE `items_strings` DROP INDEX `fullTextTitle`;
@@ -6,10 +7,15 @@ CREATE FULLTEXT INDEX `fullTextTitle` ON `items_strings`(`title`);
 ALTER TABLE `groups` DROP INDEX `fullTextName`;
 CREATE FULLTEXT INDEX `fullTextName` ON `groups`(`name`);
 
+SET GLOBAL innodb_ft_server_stopword_table = @old_stopword_table;
+
 -- +migrate Down
+SET @old_stopword_table := @@innodb_ft_server_stopword_table;
 SET GLOBAL innodb_ft_server_stopword_table = NULL;
 
 ALTER TABLE `items_strings` DROP INDEX `fullTextTitle`;
 CREATE FULLTEXT INDEX `fullTextTitle` ON `items_strings`(`title`);
 ALTER TABLE `groups` DROP INDEX `fullTextName`;
 CREATE FULLTEXT INDEX `fullTextName` ON `groups`(`name`);
+
+SET GLOBAL innodb_ft_server_stopword_table = @old_stopword_table;
