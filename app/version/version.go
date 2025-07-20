d457a6989139394900114cd77b6708287334cf0e
Dmitry Zenovich <dzenovich@gmail.com>
2025-07-21 02:15:04 +0300

    store the app version only in the app/version package as "version", add TestAddVersionHeader()

--- app/version/version.go
+++ app/version/version.go
@@ -3,14 +3,18 @@ package version
 
 import "net/http"
 
-// Version is the app version.
-var Version string
+var version = "unknown"
 
 // AddVersionHeader adds the Backend-Version header in the responses.
 func AddVersionHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Backend-Version", Version)
+		w.Header().Add("Backend-Version", GetVersion())
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+// GetVersion returns the version of the application.
+func GetVersion() string {
+	return version
 }
