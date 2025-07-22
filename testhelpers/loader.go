3305bfd7559f425417bcc2d7d0c7203577755b88
Dmitry Zenovich <dzenovich@gmail.com>
2025-07-22 17:40:57 +0300

    get rid of the global variable testhelpers.defaultGodogOptions (convert it into a getter function)

--- testhelpers/loader.go
+++ testhelpers/loader.go
@@ -15,12 +15,15 @@ import (
 	"github.com/zenovich/flowmingo"
 
 	"github.com/France-ioi/AlgoreaBackend/v2/app/appenv"
+	"github.com/France-ioi/AlgoreaBackend/v2/golang"
 )
 
-var defaultGodogOptions = godog.Options{
-	Output: colors.Colored(os.Stdout),
-	Format: "progress",
-	Strict: true,
+func defaultGodogOptions() godog.Options {
+	return godog.Options{
+		Output: colors.Colored(os.Stdout),
+		Format: "progress",
+		Strict: true,
+	}
 }
 
 func init() { //nolint:gochecknoinits // bind Godog flags only once before running tests
@@ -29,7 +32,7 @@ func init() { //nolint:gochecknoinits // bind Godog flags only once before runni
 
 // bindGodogCmdFlags binds the command arguments into the Godog options.
 func bindGodogCmdFlags() {
-	godog.BindFlags("godog.", flag.CommandLine, &defaultGodogOptions)
+	godog.BindFlags("godog.", flag.CommandLine, golang.Ptr(defaultGodogOptions()))
 }
 
 type contextKey string
@@ -47,7 +50,7 @@ func RunGodogTests(t *testing.T, tags string) {
 		panic(err)
 	}
 
-	options := defaultGodogOptions
+	options := defaultGodogOptions()
 	featureFiles := featureFilesInCurrentDir()
 
 	if tags != "" {
