682825bb02f328411d5504ec1921dadf119ca4b0
Dmitry Zenovich <dzenovich@gmail.com>
2025-08-29 23:36:39 +0300

    use MySQL named locks instead golang-implemented locking in refreshAccessToken (get rid of the global variable sessionIDsInProgress and the sessionIDsInProgressMap map, rewrite the related tests)

--- app/api/auth/refresh_access_token_test.go
+++ app/api/auth/refresh_access_token_test.go
@@ -1,12 +1,14 @@
-package auth
+//go:build !unit
+
+package auth_test
 
 import (
 	"context"
-	"io"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
-	"regexp"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -14,135 +16,157 @@ import (
 	"unsafe"
 
 	"bou.ke/monkey"
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	apiAuth "github.com/France-ioi/AlgoreaBackend/v2/app/api/auth"
 	"github.com/France-ioi/AlgoreaBackend/v2/app/auth"
 	"github.com/France-ioi/AlgoreaBackend/v2/app/database"
+	"github.com/France-ioi/AlgoreaBackend/v2/app/logging"
+	"github.com/France-ioi/AlgoreaBackend/v2/app/loggingtest"
 	"github.com/France-ioi/AlgoreaBackend/v2/app/service"
-	"github.com/France-ioi/AlgoreaBackend/v2/app/servicetest"
+	"github.com/France-ioi/AlgoreaBackend/v2/testhelpers"
 	"github.com/France-ioi/AlgoreaBackend/v2/testhelpers/testoutput"
 )
 
 func TestService_refreshAccessToken_NotAllowRefreshTokenRaces(t *testing.T) {
 	testoutput.SuppressIfPasses(t)
 
+	timePatch := testhelpers.MockDBTime("2025-08-27 22:32:01")
+	defer testhelpers.RestoreDBTime(timePatch)
+
+	db := testhelpers.SetupDBWithFixtureString(testhelpers.CreateTestContext(), `
+		access_tokens: [{token: accesstoken, session_id: 1, expires_at: "2025-08-27 22:42:01", issued_at: "2025-08-27 22:22:01"}]
+		sessions: [{session_id: 1, user_id: 2, refresh_token: "firstrefreshtoken"}]
+	`)
+	defer func() { _ = db.Close() }()
+
 	expectedClientID := "1234"
 	expectedClientSecret := "secret"
 	loginModuleStubServer := createLoginModuleStubServer(expectedClientID, expectedClientSecret)
-	done := make(chan bool)
-	doRequest := func(timeout bool) {
+	done := make(chan bool, 1)
+	lockCalledChan := make(chan struct{}, 1)
+	lockCalledByTestedCodeChan := make(chan struct{}, 1)
+	expectedLockName := fmt.Sprintf("session_%d", auth.MockCtxSessionID)
+	var funcCalled atomic.Bool
+	doRequest := func(withTimeout bool) {
 		var cancelFunc context.CancelFunc
-		if timeout {
-			var patchGuard *monkey.PatchGuard
-			patchGuard = monkey.PatchInstanceMethod(reflect.TypeOf(&sessionIDsInProgressMap{}), "WithLock",
-				func(sessionIDsInProgress *sessionIDsInProgressMap, sessionID int64, httpRequest *http.Request, funcToRun func() error) error {
+		var patchGuard *monkey.PatchGuard
+		patchGuard = monkey.PatchInstanceMethod(reflect.TypeOf(&database.DataStore{}), "WithNamedLock",
+			func(store *database.DataStore, lockName string, timeout time.Duration, funcToCall func(store *database.DataStore) error) error {
+				lockCalledByTestedCodeChan <- struct{}{} // notify that the code under test called WithNamedLock()
+				assert.Equal(t, expectedLockName, lockName)
+
+				if withTimeout {
 					cancelFunc()
-					ctx := httpRequest.Context()
+					ctx := store.GetContext()
 					(*valueCtxInterface)(unsafe.Pointer(&ctx)).p.timerCtx.err = context.DeadlineExceeded //nolint:gosec // imitate a timeout
-					patchGuard.Unpatch()
-					defer patchGuard.Restore()
-					return sessionIDsInProgress.WithLock(sessionID, httpRequest, funcToRun)
-				})
-			defer patchGuard.Unpatch()
-		}
-		response, mock, logs, err := servicetest.GetResponseForRouteWithMockedDBAndUser(
-			"POST", "/auth/token", "", &database.User{GroupID: 2},
-			func(mock sqlmock.Sqlmock) {
-				mock.ExpectQuery("^" +
-					regexp.QuoteMeta(
-						"SELECT "+
-							"token, "+
-							"TIMESTAMPDIFF(SECOND, NOW(), expires_at) AS seconds_until_expiry, "+
-							"issued_at > (NOW() - INTERVAL 5 MINUTE) AS too_new_to_refresh "+
-							"FROM `access_tokens`  WHERE (session_id = ?) ORDER BY expires_at DESC LIMIT 1") + "$").
-					WithArgs(sqlmock.AnyArg()).
-					WillReturnRows(mock.NewRows([]string{"token", "seconds_until_expiry", "too_new_to_refresh"}).
-						AddRow("accesstoken", 600, false))
-
-				if !timeout {
-					mock.ExpectQuery("^" +
-						regexp.QuoteMeta("SELECT refresh_token FROM `sessions` WHERE (session_id = ?) LIMIT 1") + "$").
-						WithArgs(sqlmock.AnyArg()).
-						WillReturnRows(mock.NewRows([]string{"refresh_token"}).AddRow("firstrefreshtoken"))
-					mock.ExpectBegin()
-					mock.ExpectExec("^"+regexp.QuoteMeta(
-						"INSERT INTO `access_tokens` (`expires_at`, `issued_at`, `session_id`, `token`) "+
-							"VALUES (NOW() + INTERVAL ? SECOND, NOW(), ?, ?)")+"$").
-						WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), "newaccesstoken").
-						WillReturnResult(sqlmock.NewResult(123, 1))
-					mock.ExpectExec("^"+regexp.QuoteMeta("UPDATE `sessions` SET `refresh_token` = ? WHERE (session_id = ?)")+"$").
-						WithArgs("newfirstrefreshtoken", sqlmock.AnyArg()).
-						WillReturnResult(sqlmock.NewResult(-1, 1))
-					mock.ExpectCommit()
-					mock.ExpectExec("^DELETE access_tokens .+$").
-						WithArgs(sqlmock.AnyArg()).
-						WillReturnResult(sqlmock.NewResult(-1, 1))
 				}
-			},
-			func(mux *chi.Mux, baseService *service.Base) {
-				srv := &Service{Base: baseService}
-				srv.AuthConfig = viper.New()
-				srv.AuthConfig.Set("loginModuleURL", loginModuleStubServer.URL)
-				srv.AuthConfig.Set("clientID", expectedClientID)
-				srv.AuthConfig.Set("clientSecret", expectedClientSecret)
-				var router chi.Router = mux
-				if timeout {
-					router = router.With(func(next http.Handler) http.Handler {
-						handlerFunc := func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
-							var ctx context.Context
-							ctx, cancelFunc = context.WithDeadline(httpRequest.Context(), time.Now().Add(1*time.Hour))
-							defer cancelFunc()
 
-							httpRequest = httpRequest.WithContext(ctx)
-							next.ServeHTTP(responseWriter, httpRequest)
-						}
-						return http.HandlerFunc(handlerFunc)
-					})
+				patchGuard.Unpatch()
+				defer patchGuard.Restore()
+				newFuncToCall := func(store *database.DataStore) error {
+					funcCalled.Store(true)
+					return funcToCall(store)
 				}
-				router.
-					With(middleware.WithValue(parsedRequestData, map[string]interface{}{})).
-					Post("/auth/token", service.AppHandler(srv.refreshAccessToken).ServeHTTP)
+				return store.WithNamedLock(lockName, timeout, newFuncToCall)
 			})
-		assert.NoError(t, err)
-		if err == nil {
-			defer func() { _ = response.Body.Close() }()
+		defer patchGuard.Unpatch()
+
+		base := service.Base{}
+		base.SetGlobalStore(database.NewDataStore(db))
+		router := chi.Router(chi.NewRouter())
+		logger, logHook := logging.NewMockLogger()
+		router.Use(logging.ContextWithLoggerMiddleware(logger))
+		router.Use(auth.MockUserMiddleware(&database.User{GroupID: 2}))
+		router.Use(middleware.RequestLogger(&logging.StructuredLogger{}))
+
+		srv := &Service{Base: &base}
+		srv.AuthConfig = viper.New()
+		srv.AuthConfig.Set("loginModuleURL", loginModuleStubServer.URL)
+		srv.AuthConfig.Set("clientID", expectedClientID)
+		srv.AuthConfig.Set("clientSecret", expectedClientSecret)
+		routerForAddingHandlers := router
+		if withTimeout {
+			routerForAddingHandlers = routerForAddingHandlers.With(func(next http.Handler) http.Handler {
+				handlerFunc := func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
+					var ctx context.Context
+					ctx, cancelFunc = context.WithDeadline(httpRequest.Context(), time.Now().Add(1*time.Hour))
+					defer cancelFunc()
+
+					httpRequest = httpRequest.WithContext(ctx)
+					next.ServeHTTP(responseWriter, httpRequest)
+				}
+				return http.HandlerFunc(handlerFunc)
+			})
 		}
-		if timeout {
-			assert.Equal(t, 408, response.StatusCode)
-			assert.Contains(t, logs, "The request is canceled: context deadline exceeded")
+
+		routerForAddingHandlers.
+			With(func(next http.Handler) http.Handler {
+				fn := func(w http.ResponseWriter, r *http.Request) {
+					r = r.WithContext(contextWithParsedRequestData(r.Context(), map[string]interface{}{}))
+					next.ServeHTTP(w, r)
+				}
+				return http.HandlerFunc(fn)
+			}).Post("/auth/token", service.AppHandler(srv.refreshAccessToken).ServeHTTP)
+
+		request, err := http.NewRequest(http.MethodPost, "/auth/token", strings.NewReader(""))
+		require.NoError(t, err)
+
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		logs := (&loggingtest.Hook{Hook: logHook}).GetAllLogs()
+
+		if withTimeout {
+			assert.Equal(t, 408, recorder.Code)
+			assert.Contains(t, logs, "context deadline exceeded")
 		} else {
-			assert.Equal(t, 201, response.StatusCode)
-			body, _ := io.ReadAll(response.Body)
+			assert.Equal(t, 201, recorder.Code)
+			body := recorder.Body.String()
 			assert.JSONEq(t,
 				`{"success":true,"message":"created","data":{"access_token":"newaccesstoken","expires_in":78901234}}`,
-				string(body))
+				body)
 		}
-		assert.NoError(t, mock.ExpectationsWereMet())
-		done <- true
+		done <- true // notify that doRequest() has finished
 	}
 
-	// check that the service waits while the session is locked
-	mutexChannel := make(chan bool, 1)
-	(*sync.Map)(&sessionIDsInProgress).Store(auth.MockCtxSessionID, mutexChannel) // lock the session
-	mutexChannel <- true
-	go doRequest(false)
-	mutexChannel <- true // wait until refreshAccessToken() reads from the channel (meaning the service is inside the for loop)
-	close(mutexChannel)
-	(*sync.Map)(&sessionIDsInProgress).Delete(auth.MockCtxSessionID) // here the service gets unlocked
-	<-done                                                           // wait until the service finishes
+	store := database.NewDataStore(db)
+
+	// lock the session
+	go func() {
+		assert.NoError(t, store.WithNamedLock(expectedLockName, 1*time.Second, func(_ *database.DataStore) error {
+			lockCalledChan <- struct{}{} // notify that the lock has been acquired by the test
+			<-lockCalledByTestedCodeChan // wait for the code under test to call WithNamedLock()
+			return nil                   // unlock letting doRequest() to perform
+		}))
+	}()
+	<-lockCalledChan // wait for the concurrent test function to acquire the lock
+	doRequest(false)
+	<-done // wait until the service finishes
+	assert.True(t, funcCalled.Load())
+
+	funcCalled.Store(false)
+	require.NoError(t, store.AccessTokens().Delete("token = 'newaccesstoken'").Error())
 
 	// check that the service timeouts if the session is locked for too long
-	mutexChannel = make(chan bool, 1)
-	(*sync.Map)(&sessionIDsInProgress).Store(auth.MockCtxSessionID, mutexChannel) // lock the session
-	// Remove the mutex once we're finished, otherwise it makes further tests block if they use the same session ID.
-	defer (*sync.Map)(&sessionIDsInProgress).Delete(auth.MockCtxSessionID)
-	mutexChannel <- true
-	go doRequest(true)
-	<-done // wait until the service finishes
+
+	// lock the session
+	go func() {
+		assert.NoError(t, store.WithNamedLock(expectedLockName, 1*time.Second, func(_ *database.DataStore) error {
+			lockCalledChan <- struct{}{} // notify that the lock has been acquired by the test
+			<-done                       // wait until the service finishes with timeout
+			return nil                   // unlock
+		}))
+	}()
+
+	<-lockCalledChan // wait for the concurrent test function to acquire the lock
+	doRequest(true)
+	<-lockCalledByTestedCodeChan // be sure the code under test has called WithNamedLock()
+	assert.False(t, funcCalled.Load())
 }
 
 func createLoginModuleStubServer(expectedClientID, expectedClientSecret string) *httptest.Server {
@@ -181,3 +205,17 @@ type valueCtxInterface struct {
 	t unsafe.Pointer
 	p *valueCtx
 }
+
+type Service struct {
+	*service.Base
+}
+
+func (srv *Service) refreshAccessToken(w http.ResponseWriter, r *http.Request) error {
+	return srvRefreshAccessToken((*apiAuth.Service)(srv), w, r)
+}
+
+//go:linkname srvRefreshAccessToken github.com/France-ioi/AlgoreaBackend/v2/app/api/auth.(*Service).refreshAccessToken
+func srvRefreshAccessToken(srv *apiAuth.Service, w http.ResponseWriter, r *http.Request) error
+
+//go:linkname contextWithParsedRequestData github.com/France-ioi/AlgoreaBackend/v2/app/api/auth.contextWithParsedRequestData
+func contextWithParsedRequestData(ctx context.Context, requestData map[string]interface{}) context.Context
