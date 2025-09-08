4301635b271343ae9d004b3591d4bcc7fbae7869
Dmitry Zenovich <dzenovich@gmail.com>
2025-09-09 00:59:32 +0300

    mention in the DB comments that groups_groups.child_group_type & groups_ancestors.child_group_type duplicate groups.type for the sake of performance

--- db/migrations/2509082013_add_column_groups_ancestors_child_group_type.sql
+++ db/migrations/2509082013_add_column_groups_ancestors_child_group_type.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 ALTER TABLE `groups_ancestors`
   ADD COLUMN `child_group_type` ENUM('Class','Team','Club','Friends','Other','User','Session','Base','ContestParticipants') DEFAULT NULL
-  COMMENT 'The type of the child group in the relationship' AFTER `child_group_id`;
+  COMMENT 'The type of the child group in the relationship (we duplicate groups.type to improve performance)' AFTER `child_group_id`;
 
 -- +goose Down
 ALTER TABLE `groups_ancestors`
